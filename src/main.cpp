7a651300dbbf4b454205e21f178496fb5bc2e9ff
roynatech2544 <whiteshell2544@naver.com>
2023-02-25 13:04:17 +0900

    tgcpp_host: Disable spamdetect config

--- src/main.cpp
+++ src/main.cpp
@@ -401,7 +401,13 @@ int main(void) {
         static std::mutex m;
         static std::atomic_bool cb;
         static bool falseth;
+	static std::ifstream config;
 
+	config.open(".spamdetectdisabled");
+	if (config.good()) {
+		config.close();
+		return;
+	}
         if (std::time(0) - message->date > 10) return;
         if (!falseth) {
             std::thread([cb = &cb]() {
