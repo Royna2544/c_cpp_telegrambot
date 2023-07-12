36ec31a81f61939421668c9051ccea8c2f76f592
roynatech2544 <whiteshell2544@naver.com>
2023-07-12 22:20:02 +0900

    tg_cpphost: Timer: Combine code to one class definition

--- src/timer/Timer.h
+++ src/timer/Timer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <chrono>
 #include <functional>
 #include <memory>
 #include <thread>
@@ -31,9 +32,6 @@ class Timer {
     Timer() = delete;
     Timer(unsigned int h, unsigned int m, unsigned int s) : h(h), m(m), s(s){};
 
-    void setCallback(const time_callback_t<T> onEvery, const unsigned int onsec,
-                     const callback_t<T> onEnd, const std::unique_ptr<T> priv);
-    void start(void);
     bool cancel(const cancel_validator_t<T> cancel) {
         bool shouldcancel = cancel && cancel(priv.get());
         if (shouldcancel) stop = true;
@@ -41,47 +39,42 @@ class Timer {
     }
     void cancel(void) { stop = true; }
     bool isrunning(void) { return !stop; }
-};
+    void setCallback(const time_callback_t<T> onEvery,
+                     const unsigned int onsec, const callback_t<T> onEnd,
+                     const std::unique_ptr<T> priv) {
+        this->onEvery = onEvery;
+        this->onsec = onsec;
+        this->onEnd = onEnd;
+        this->priv = std::make_unique<T>(*priv);
+    }
 
-#include <chrono>
+    void start(void) {
+        if (s >= 60) s = 59;
+        if (m >= 60) m = 59;
+        stop = false;
 
-template <typename T>
-void Timer<T>::setCallback(const time_callback_t<T> onEvery,
-                           const unsigned int onsec, const callback_t<T> onEnd,
-                           const std::unique_ptr<T> priv) {
-    this->onEvery = onEvery;
-    this->onsec = onsec;
-    this->onEnd = onEnd;
-    this->priv = std::make_unique<T>(*priv);
-}
+        std::thread([=]() {
+            int h_ = h, m_ = m, s_ = s;
 
-template <typename T>
-void Timer<T>::start(void) {
-    if (s >= 60) s = 59;
-    if (m >= 60) m = 59;
-    stop = false;
-
-    std::thread([=]() {
-        int h_ = h, m_ = m, s_ = s;
-
-        while (h_ >= 0) {
-            while (m_ >= 0) {
-                while (s_ >= 0) {
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
+            while (h_ >= 0) {
+                while (m_ >= 0) {
+                    while (s_ >= 0) {
+                        std::this_thread::sleep_for(std::chrono::seconds(1));
+                        if (stop) break;
+                        if (onEvery && s_ % onsec == 0)
+                            onEvery(priv.get(), {h_, m_, s_});
+                        s_--;
+                    }
                     if (stop) break;
-                    if (onEvery && s_ % onsec == 0)
-                        onEvery(priv.get(), {h_, m_, s_});
-                    s_--;
+                    if (m_ != 0 && s_ == -1) s_ = 60 - onsec;
+                    m_--;
                 }
                 if (stop) break;
-                if (m_ != 0 && s_ == -1) s_ = 60 - onsec;
-                m_--;
+                if (h_ != 0 && m_ == -1) m_ = 60;
+                h_--;
             }
-            if (stop) break;
-            if (h_ != 0 && m_ == -1) m_ = 60;
-            h_--;
-        }
-        if (onEnd) onEnd(priv.get());
-        stop = true;
-    }).detach();
-}
+            if (onEnd) onEnd(priv.get());
+            stop = true;
+        }).detach();
+    }
+};
