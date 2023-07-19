d4d762adca5cbdff471ee497ca45686c8e8887d0
roynatech2544 <whiteshell2544@naver.com>
2023-07-19 11:22:46 +0900

    scripts: Add deps install script

---
+++ scripts/deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APT_COMMAND="apt install libcurl4-openssl-dev libssl-dev libboost-all-dev cmake clang"
+
+function lecho () {
+    echo "$0: $1"
+}
+
+trap 'lecho "Exiting script with $?"' EXIT
+
+if [ ! -x /usr/bin/apt ]; then
+    lecho "Only Debian-based are supported for now (apt)"
+    exit 1;
+fi
+
+if [ $UID -eq 0 ]; then
+    lecho "Current user is root"
+    eval $APT_COMMAND
+else
+    lecho "Current user is not root, trying sudo"
+    eval "sudo $APT_COMMAND" || lecho "Failed to get root"
+fi
