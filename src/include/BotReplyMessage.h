070ba8e2aad378f39c73cf371a7968943a8a58db
roynatech2544 <whiteshell2544@naver.com>
2023-07-29 14:46:09 +0900

    tg_cpphost: bot_sendReplyMsg: Allow sendwoerror partially

--- src/include/BotReplyMessage.h
+++ src/include/BotReplyMessage.h
@@ -10,7 +10,9 @@ using TgBot::Message;
 using TgBot::MessageEntity;
 
 static inline void bot_sendReplyMessage(const Bot &bot, const Message::Ptr &message,
-                                        const std::string &text, const int32_t replyToMsg = 0) {
+                                        const std::string &text, const int32_t replyToMsg = 0,
+                                        const bool noError = false) {
     bot.getApi().sendMessage(message->chat->id, text,
-                             true, (replyToMsg == 0) ? message->messageId : replyToMsg);
+                             true, (replyToMsg == 0) ? message->messageId : replyToMsg,
+                             nullptr, "", false, std::vector<MessageEntity::Ptr>(), noError);
 }
