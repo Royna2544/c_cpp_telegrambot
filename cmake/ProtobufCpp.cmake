48f8ad84258ea31f286d5dd59843923aeb5474a1
roynatech2544 <whiteshell2544@naver.com>
2023-07-29 13:28:37 +0900

    tg_cpphost: CMake: format
    - https://github.com/cheshirekow/cmake_format

--- cmake/ProtobufCpp.cmake
+++ cmake/ProtobufCpp.cmake
@@ -1,6 +1,8 @@
 function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
   if(NOT ARGN)
-    message(SEND_ERROR "Error: PROTOBUF_GENERATE_CPP() called without any proto files")
+    message(
+      SEND_ERROR "Error: PROTOBUF_GENERATE_CPP() called without any proto files"
+    )
     return()
   endif()
 
@@ -11,7 +13,7 @@ function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
       get_filename_component(ABS_PATH ${ABS_FIL} PATH)
       list(FIND _protobuf_include_path ${ABS_PATH} _contains_already)
       if(${_contains_already} EQUAL -1)
-          list(APPEND _protobuf_include_path -I ${ABS_PATH})
+        list(APPEND _protobuf_include_path -I ${ABS_PATH})
       endif()
     endforeach()
   else()
@@ -23,7 +25,7 @@ function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
       get_filename_component(ABS_PATH ${DIR} ABSOLUTE)
       list(FIND _protobuf_include_path ${ABS_PATH} _contains_already)
       if(${_contains_already} EQUAL -1)
-          list(APPEND _protobuf_include_path -I ${ABS_PATH})
+        list(APPEND _protobuf_include_path -I ${ABS_PATH})
       endif()
     endforeach()
   endif()
@@ -37,19 +39,25 @@ function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
     list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${FIL_WE}.pb.cc")
     list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${FIL_WE}.pb.h")
 
-    execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/${PATH})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory
+                            ${CMAKE_CURRENT_BINARY_DIR}/${PATH})
 
     add_custom_command(
       OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${FIL_WE}.pb.cc"
              "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${FIL_WE}.pb.h"
-      COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR}/${PATH} ${_protobuf_include_path} ${ABS_FIL}
+      COMMAND
+        ${PROTOBUF_PROTOC_EXECUTABLE} ARGS --cpp_out
+        ${CMAKE_CURRENT_BINARY_DIR}/${PATH} ${_protobuf_include_path} ${ABS_FIL}
       DEPENDS ${ABS_FIL}
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
-      VERBATIM )
+      VERBATIM)
   endforeach()
 
   set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
-  set(${SRCS} ${${SRCS}} PARENT_SCOPE)
-  set(${HDRS} ${${HDRS}} PARENT_SCOPE)
+  set(${SRCS}
+      ${${SRCS}}
+      PARENT_SCOPE)
+  set(${HDRS}
+      ${${HDRS}}
+      PARENT_SCOPE)
 endfunction()
