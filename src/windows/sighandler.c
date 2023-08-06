8477df100f4a1b6495d2dd9d27766359c35da4f7
roynatech2544 <whiteshell2544@naver.com>
2023-08-06 13:17:22 +0900

    tg_cpphost: windows: Format sighandler

--- src/windows/sighandler.c
+++ src/windows/sighandler.c
@@ -2,21 +2,19 @@
 
 static void (*cleanupFn)() = NULL;
 
-static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
-{
-    switch (fdwCtrlType)
-    {
-    case CTRL_C_EVENT:
-    case CTRL_CLOSE_EVENT:
-    case CTRL_BREAK_EVENT:
-    case CTRL_LOGOFF_EVENT:
-    case CTRL_SHUTDOWN_EVENT:
-        if (cleanupFn)
-            cleanupFn();
-        return FALSE;
+static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType) {
+    switch (fdwCtrlType) {
+        case CTRL_C_EVENT:
+        case CTRL_CLOSE_EVENT:
+        case CTRL_BREAK_EVENT:
+        case CTRL_LOGOFF_EVENT:
+        case CTRL_SHUTDOWN_EVENT:
+            if (cleanupFn)
+                cleanupFn();
+            return FALSE;
 
-    default:
-        return FALSE;
+        default:
+            return FALSE;
     }
 }
 
