69f466f1a73c706a9d343458d24a36ddb226f6fa
roynatech2544 <whiteshell2544@naver.com>
2023-10-16 17:07:41 +0900

    tg_cpphost: Add protobuf database dumper tool
    - Export popen_wdt, libutils to libTgBotUtils

---
+++ proto/DumpProtoDB.cc
@@ -0,0 +1,34 @@
+#include <Database.h>
+#include <TgBotDB.pb.h>
+
+#include <iostream>
+
+using database::db;
+using tgbot::proto::PersonList;
+
+static void dumpList(const PersonList& list, const char* name) {
+  const int id_size = list.id_size();
+  std::cout << "Dump of " << name << std::endl;
+  std::cout << "Size: " << id_size << std::endl;
+  if (id_size > 0) {
+    for (int i = 0; i < id_size; i++)
+      std::cout << "- " << list.id(i) << std::endl;
+    std::cout << std::endl;
+  }
+}
+
+int main(void) {
+  std::cout << "Owner ID: ";
+  if (db->has_ownerid()) {
+    std::cout << db->ownerid();
+  } else {
+    std::cout << "Not set";
+  }
+  std::cout << std::endl;
+  if (db->has_whitelist()) {
+    dumpList(db->whitelist(), "whitelist");
+  }
+  if (db->has_blacklist()) {
+    dumpList(db->blacklist(), "blacklist");
+  }
+}
