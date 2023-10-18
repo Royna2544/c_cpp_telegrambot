d7e75bbf2d71e73139aee2f247434e472c1f8259
roynatech2544 <whiteshell2544@naver.com>
2023-10-18 21:24:41 +0900

    tg_cpphost: BotReplyMessage: return result of sent msg

--- src/include/BotReplyMessage.h
+++ src/include/BotReplyMessage.h
@@ -9,10 +9,10 @@ using TgBot::Bot;
 using TgBot::Message;
 using TgBot::MessageEntity;
 
-static inline void bot_sendReplyMessage(const Bot &bot, const Message::Ptr &message,
+static inline Message::Ptr bot_sendReplyMessage(const Bot &bot, const Message::Ptr &message,
                                         const std::string &text, const int32_t replyToMsg = 0,
                                         const bool noError = false) {
-    bot.getApi().sendMessage(message->chat->id, text,
+    return bot.getApi().sendMessage(message->chat->id, text,
                              true, (replyToMsg == 0) ? message->messageId : replyToMsg,
                              nullptr, "", false, std::vector<MessageEntity::Ptr>(), noError);
 }
