b9bb179509dee8d30e726c9287646a2fbd3b118e
roynatech2544 <whiteshell2544@naver.com>
2023-10-25 19:58:02 +0900

    tg_cpphost: cmd_dynamic: Address a warning

--- src/cmd_dynamic/cmd_dynamic.h
+++ src/cmd_dynamic/cmd_dynamic.h
@@ -2,15 +2,17 @@
 
 struct dynamicCommand {
     bool enforced;
-    const char *name;
+    const char* name;
     void (*fn)(const Bot& bot, const Message::Ptr& message);
 };
 
 #define DYN_COMMAND_SYM cmd
 #define DYN_COMMAND_SYM_STR "cmd"
-#define DECL_DYN_COMMAND(enf, name_, fn_)  \
-    extern "C" struct dynamicCommand DYN_COMMAND_SYM { \
-        .enforced = enf,                   \
-        .name = name_,                     \
-        .fn = fn_,                         \
-    };
+#define DECL_DYN_COMMAND(enf, name_, fn_)   \
+    extern "C" {                            \
+    struct dynamicCommand DYN_COMMAND_SYM { \
+        .enforced = enf,                    \
+        .name = name_,                      \
+        .fn = fn_,                          \
+    };                                      \
+    }
