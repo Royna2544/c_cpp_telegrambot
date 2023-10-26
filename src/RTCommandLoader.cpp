3498136c674012e64e6b6602aa78dcda8fef15b5
roynatech2544 <whiteshell2544@naver.com>
2023-10-26 23:06:25 +0900

    tg_cpphost: cmd_dynamic: Show unsupported state on cmd

--- src/RTCommandLoader.cpp
+++ src/RTCommandLoader.cpp
@@ -18,6 +18,10 @@ struct DynamicLibraryHolder {
 
 static std::vector<DynamicLibraryHolder> libs;
 
+static void commandStub(const Bot& bot, const Message::Ptr& message) {
+    bot_sendReplyMessage(bot, message, "Unsupported command");
+}
+
 void loadOneCommand(Bot& bot, const std::string& fname) {
     void* handle = dlopen(fname.c_str(), RTLD_NOW);
     if (!handle) {
@@ -27,19 +31,20 @@ void loadOneCommand(Bot& bot, const std::string& fname) {
     struct dynamicCommand* sym = (struct dynamicCommand*)dlsym(handle, DYN_COMMAND_SYM_STR);
     if (!sym) {
         LOG_W("Failed to lookup symbol '" DYN_COMMAND_SYM_STR "' in %s", fname.c_str());
-	dlclose(handle);
+        dlclose(handle);
         return;
     }
+    libs.emplace_back(handle);
+    auto fn = sym->fn;
     if (!sym->isSupported()) {
-	LOG_I("Module %s declares it is not supported.", fname.c_str());
-	dlclose(handle);
-	return;
+        LOG_I("Module declares it is not supported.");
+        fn = commandStub;
     }
     libs.emplace_back(handle);
     if (sym->enforced)
-        bot_AddCommandEnforced(bot, sym->name, sym->fn);
+        bot_AddCommandEnforced(bot, sym->name, fn);
     else
-        bot_AddCommandPermissive(bot, sym->name, sym->fn);
+        bot_AddCommandPermissive(bot, sym->name, fn);
     LOG_I("Loaded RT command module from %s", fname.c_str());
     LOG_I("Module dump: { enforced: %d, name: %s, fn: %p }", sym->enforced, sym->name, sym->fn);
 }
