e70a1b9446d764dbf4484c47602d4791ce5f4faa
roynatech2544 <whiteshell2544@naver.com>
2023-11-11 16:34:09 +0900

    tg_cpphost: SocketHandler: Catch possible exception while sending msg
    - e.g. Wrong chatid, that shouldn't crash the main bot

--- src/SocketConnectionHandler.cpp
+++ src/SocketConnectionHandler.cpp
@@ -6,7 +6,11 @@ void socketConnectionHandler(const Bot& bot, struct TgBotConnection conn) {
     auto _data = conn.data;
     switch (conn.cmd) {
         case CMD_WRITE_MSG_TO_CHAT_ID:
-            bot.getApi().sendMessage(_data.data_1.to, _data.data_1.msg);
+            try {
+                bot.getApi().sendMessage(_data.data_1.to, _data.data_1.msg);
+            } catch (const TgBot::TgException& e) {
+                LOG_E("Exception at handler, %s", e.what());
+            }
             break;
         case CMD_CTRL_SPAMBLOCK:
             gSpamBlockCfg = _data.data_3;
