8b0611ad37c25575288b1d205bafe8068b8d8919
roynatech2544 <whiteshell2544@naver.com>
2023-11-19 11:54:50 +0900

    tg_cpphost: BotAddCommand: Provide a stub if compiler not supported

--- src/BotAddCommand.cpp
+++ src/BotAddCommand.cpp
@@ -1,6 +1,10 @@
 #include <Authorization.h>
 #include <BotAddCommand.h>
 
+static void NoCompilerCommandStub(const Bot& bot, const Message::Ptr& message) {
+    bot_sendReplyMessage(bot, message, "Not supported in current host");
+}
+
 void bot_AddCommandPermissive(Bot& bot, const char* cmd, command_callback_t cb) {
     auto authFn = [&, cb](const Message::Ptr message) {
         if (!Authorized(message, true, true)) return;
@@ -24,5 +28,6 @@ void bot_AddCommandEnforcedCompiler(Bot& bot, const char* cmd,
         bot_AddCommandEnforced(bot, cmd, std::bind(cb, pholder1, pholder2, compiler));
     } else {
         LOG_W("Unsupported cmd '%s' (compiler)", cmd);
+        bot_AddCommandEnforced(bot, cmd, NoCompilerCommandStub);
     }
 }
