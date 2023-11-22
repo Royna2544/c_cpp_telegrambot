a575f787a0690ad4d2919eaaa6f8fae2f2951b96
roynatech2544 <whiteshell2544@naver.com>
2023-11-22 14:11:34 +0900

    tg_cpphost: RegEXHandler: Add a OptionWrapper class for clean syntax

--- src/RegEXHandler.cpp
+++ src/RegEXHandler.cpp
@@ -20,6 +20,35 @@ static std::regex kSedReplaceCommandRegex(R"(^s\/.+\/.+(\/(g|i|ig|gi))?$)");
 // Matches sed command with delete command, with regex on deleting expression
 static std::regex kSedDeleteCommandRegex(R"(^\/.+\/d$)");
 
+template<typename T>
+class OptionalWrapper {
+    std::optional<T> val;
+ public:
+    OptionalWrapper<T>(const std::optional<T> _val) : val(_val) {}
+    OptionalWrapper<T>() : val(std::nullopt) {}
+
+    bool has_value() const {
+        return val.has_value();
+    }
+    T value() const {
+        return val.value();
+    }
+    T operator*() const {
+        return value();
+    }
+    operator bool() const {
+        return has_value();
+    }
+    OptionalWrapper<T> operator |= (OptionalWrapper<T> &&other) {
+        if (this != &other) {
+            if (other.has_value() && !has_value()) {
+                val = std::move(other.val);
+            }
+        }
+        return *this;
+    }
+};
+
 static std::vector<std::string> matchAndSplit(const Bot& bot, const Message::Ptr msg, const std::regex& regex) {
     std::string& text = msg->text;
     if (std::regex_match(text, regex)) {
@@ -42,7 +71,7 @@ static std::optional<std::regex> safeConstructRegex(const Bot& bot, const Messag
     }
 }
 
-static std::optional<std::string> doRegexReplaceCommand(const Bot& bot, const Message::Ptr& msg) {
+static OptionalWrapper<std::string> doRegexReplaceCommand(const Bot& bot, const Message::Ptr& msg) {
     std::regex src;
     std::string dest;
     bool global = false;  // g flag in sed
@@ -71,17 +100,17 @@ static std::optional<std::string> doRegexReplaceCommand(const Bot& bot, const Me
             LOG_D("src: '%s' dest: '%s' global: %d icase: %d", args[1].c_str(), args[2].c_str(),
                   global, kRegexFlags & icase);
             try {
-                return std::regex_replace(msg->replyToMessage->text, src, dest, kRegexMatchFlags);
+                return {std::regex_replace(msg->replyToMessage->text, src, dest, kRegexMatchFlags)};
             } catch (const std::regex_error& e) {
                 bot_sendReplyMessage(bot, msg, std::string() + "Exception while executing doRegex: " + e.what());
             }
         }
     }
 
-    return std::nullopt;
+    return {std::nullopt};
 }
 
-static std::optional<std::string> doRegexDeleteCommand(const Bot& bot, const Message::Ptr& msg) {
+static OptionalWrapper<std::string> doRegexDeleteCommand(const Bot& bot, const Message::Ptr& msg) {
     auto args = matchAndSplit(bot, msg, kSedDeleteCommandRegex);
     // /aaaa/d
     if (args.size() == 3) {
@@ -94,22 +123,16 @@ static std::optional<std::string> doRegexDeleteCommand(const Bot& bot, const Mes
                     kOutStream << line << std::endl;
                 }
             }
-            return kOutStream.str();
+            return {kOutStream.str()};
         }
     }
-    return std::nullopt;
+    return {std::nullopt};
 }
-void processRegEXCommand(const Bot& bot, const Message::Ptr& msg) {
-    std::optional<std::string> ret;
 
-    do {
-        ret = doRegexReplaceCommand(bot, msg);
-        if (ret)
-            break;
-        ret = doRegexDeleteCommand(bot, msg);
-        if (ret)
-            break;
-        return;
-    } while (0);
-    bot_sendReplyMessage(bot, msg, *ret);
+void processRegEXCommand(const Bot& bot, const Message::Ptr& msg) {
+    OptionalWrapper<std::string> ret;
+    ret |= doRegexReplaceCommand(bot, msg);
+    ret |= doRegexDeleteCommand(bot, msg);
+    if (ret)
+        bot_sendReplyMessage(bot, msg, *ret);
 }
