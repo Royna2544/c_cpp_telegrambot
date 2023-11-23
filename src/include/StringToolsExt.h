78b32f2ad96691f799354b65b4179f32e140de9a
roynatech2544 <whiteshell2544@naver.com>
2023-11-23 19:58:53 +0900

    tg_cpphost: StringToolsExt: Add boost::trim alias

--- src/include/StringToolsExt.h
+++ src/include/StringToolsExt.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <algorithm>
+#include <boost/algorithm/string/trim.hpp>
 #include <string>
 
 static inline bool isEmptyOrBlank(const std::string& str) {
     return str.empty() || std::all_of(str.begin(), str.end(),
                                       [](char c) { return std::isspace(c); });
+}
+
+static inline void TrimStr(std::string& str) {
+    boost::trim(str);
 }
