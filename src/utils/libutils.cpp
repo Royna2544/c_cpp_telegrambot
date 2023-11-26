4bdf303816497fe48c56167843554a54556aa574
roynatech2544 <whiteshell2544@naver.com>
2023-11-26 09:53:05 +0900

    tg_cpphost: utils: Warn if rng is pesudo

--- src/utils/libutils.cpp
+++ src/utils/libutils.cpp
@@ -110,6 +110,8 @@ std::string findCompiler(ProgrammingLangs lang) {
 int genRandomNumber(const int min, const int max) {
     std::random_device rd;
     std::mt19937 gen(rd());
+
+    WARN_ONCE(rd.entropy() == 0, "Pesudo RNG detected");
     if (min >= max) {
         throw runtime_errorf("min(%d) >= max(%d)", min, max);
     }
