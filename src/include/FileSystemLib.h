491318ac99b1107e1dad9bf4e1054f3741eeb042
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-14 22:25:03 +0900

    TgBot++: FsLib: Drop dir_delimiter

--- src/include/FileSystemLib.h
+++ src/include/FileSystemLib.h
@@ -4,14 +4,9 @@
 
 #ifdef __WIN32
 static inline const char path_env_delimiter = ';';
-static inline const char dir_delimiter = '\\';
 #else
 static inline const char path_env_delimiter = ':';
-static inline const char dir_delimiter = '/';
 #endif
-
-// TODO Find some way to sync with above
-static inline const char unix_dir_delimiter = '/';
 
 // Implemented sperately by OS
 bool canExecute(const std::string& path);
