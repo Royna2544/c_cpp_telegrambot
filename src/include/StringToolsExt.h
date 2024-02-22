0250087668de12c70801df778f7e550fb79798db
roynatech2544 <whiteshell2544@naver.com>
2024-02-22 17:08:47 +0900

    TgBot++: StringToolsExt: Allow compilation without tgbot lib

--- src/include/StringToolsExt.h
+++ src/include/StringToolsExt.h
@@ -2,10 +2,7 @@
 
 #include <algorithm>
 #include <boost/algorithm/string/trim.hpp>
-#include <vector>
 #include <string>
-
-#include <tgbot/tools/StringTools.h>
 
 static inline bool isEmptyOrBlank(const std::string& str) {
     return str.empty() || std::all_of(str.begin(), str.end(),
@@ -16,9 +13,14 @@ static inline void TrimStr(std::string& str) {
     boost::trim(str);
 }
 
+#if __has_include(<tgbot/tools/StringTools.h>)
+#include <vector>
+#include <tgbot/tools/StringTools.h>
+
 static inline void splitAndClean(const std::string& text, std::vector<std::string>& out) {
     auto v = StringTools::split(text, '\n');
     auto it = std::remove_if(v.begin(), v.end(), [](const std::string& s) { return isEmptyOrBlank(s); });
     v.erase(it, v.end());
     out = v;
 }
+#endif
