7a7a428e9368630bda80d29237d63d70573c9d97
roynatech2544 <whiteshell2544@naver.com>
2024-02-25 17:28:26 +0900

    TgBot++: ConfigManager: Add PATH var, override support

--- src/ConfigManager.cpp
+++ src/ConfigManager.cpp
@@ -3,23 +3,20 @@
 #include <Logging.h>
 
 #include <boost/program_options.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/variables_map.hpp>
 #include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <functional>
-#include <map>
-#include <mutex>
-#include <stdexcept>
-#include "StringToolsExt.h"
 
 struct ConfigBackendBase {
     std::function<void *(void)> load;
     std::function<bool(const void *priv, const std::string &name, std::string &outvalue)> getVariable;
+    std::function<bool(const void *priv, const std::string& config)> doOverride;
     const char *name;
-    void *priv;
+    struct {
+        void *data = nullptr;
+        bool initialized = false;
+    } priv;
 };
 
 static bool env_getVariable(const void *, const std::string &name, std::string &outvalue) {
@@ -33,6 +30,7 @@ static bool env_getVariable(const void *, const std::string &name, std::string &
 
 namespace po = boost::program_options;
 struct file_priv {
+    constexpr static const char kConfigOverrideVar[] = "OVERRIDE_CONF";
     po::variables_map mp;    
 };
 
@@ -54,7 +52,10 @@ static void *file_load(void) {
     }
     desc.add_options()
         ("TOKEN", "Bot Token")
-        ("SRC_ROOT", "Root directory of source tree");
+        ("SRC_ROOT", "Root directory of source tree")
+        ("PATH", "Enviroment variable PATH")
+        (file_priv::kConfigOverrideVar, po::value<std::vector<std::string>>()->multitoken(),
+            "Config list to override to file");
     po::store(po::parse_config_file(ifs, desc), p.mp);
     po::notify(p.mp);
     
@@ -64,10 +65,21 @@ static void *file_load(void) {
 
 static bool file_getVariable(const void *p, const std::string &name, std::string &outvalue) {
     auto priv = reinterpret_cast<const file_priv *>(p);
-    if (priv) {
+    if (priv && name != file_priv::kConfigOverrideVar) {
         if (const auto it = priv->mp[name]; !it.empty()) {
             outvalue = it.as<std::string>();
             return true;
+        }
+    }
+    return false;
+}
+
+static bool file_doOverride(const void *p, const std::string& name) {
+    auto priv = reinterpret_cast<const file_priv *>(p);
+    if (priv) {
+        if (const auto it = priv->mp[file_priv::kConfigOverrideVar]; !it.empty()) {
+            const auto vec = it.as<std::vector<std::string>>();
+            return std::find(vec.begin(), vec.end(), name) != vec.end();
         }
     }
     return false;
@@ -81,44 +93,40 @@ static struct ConfigBackendBase backends[] = {
     {
         .load = file_load,
         .getVariable = file_getVariable,
+        .doOverride = file_doOverride,
         .name = "File",
     }};
 
 namespace ConfigManager {
 
-static bool load(void) {
-    static std::once_flag once;
-    try {
-        std::call_once(once, [] {
-            for (size_t i = 0; i < sizeof(backends) / sizeof(ConfigBackendBase); ++i) {
-                auto ptr = &backends[i];
-                if (!ptr->getVariable) throw std::runtime_error("Not ready");
-                if (ptr->load)
-                    ptr->priv = ptr->load();
-                else
-                    ptr->priv = nullptr;
-            }
-        });
-    } catch (const std::runtime_error &) {
-        return false;
-    }
-    return true;
-}
-
 bool getVariable(const std::string &name, std::string &outvalue) {
     ConfigBackendBase *ptr;
+    int once_flag = 0;
 
-    if (load()) {
+    do {
         for (size_t i = 0; i < sizeof(backends) / sizeof(ConfigBackendBase); ++i) {
             ptr = &backends[i];
-            if (ptr->getVariable(ptr->priv, name, outvalue)) {
+            if (!ptr->priv.initialized) {
+                if (ptr->load)
+                    ptr->priv.data = ptr->load();
+                ptr->priv.initialized = true;
+            }
+            ASSERT(ptr->getVariable, "Bad: getVariable not yet set");
+            if (once_flag == 0) {
+                if (ptr->doOverride && ptr->doOverride(ptr->priv.data, name)) {
+                    LOG_V("Used '%s' backend for fetching var '%s' (forced)", ptr->name, name.c_str());
+                    return ptr->getVariable(ptr->priv.data, name, outvalue);
+                }
+                continue;
+            }
+            if (ptr->getVariable(ptr->priv.data, name, outvalue)) {
                 LOG_V("Used '%s' backend for fetching var '%s'", ptr->name, name.c_str());
                 return true;
             }
         }
-    } else {
-        LOG_E("Manager not ready: while loading '%s'", name.c_str());
-    }
+        ++once_flag;
+    } while (once_flag < 2);
+
     return false;
 }
 }  // namespace ConfigManager
