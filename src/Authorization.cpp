a2e1aa7cfb2f70e36460305d57155bd9f46c71b2
roynatech2544 <whiteshell2544@naver.com>
2024-02-25 16:35:12 +0900

    TgBot++: Authorization: Unify single-usage function

--- src/Authorization.cpp
+++ src/Authorization.cpp
@@ -1,23 +1,20 @@
 #include <Authorization.h>
 #include <Database.h>
 #include <Types.h>
-#include <chrono>
 
 bool gAuthorized = true;
 
-static bool AuthorizedId(const UserId id, const bool permissive) {
-    if (!permissive) {
-        if (database::whitelist.exists(id)) return true;
-        return id == database::db.maybeGetOwnerId();
-    } else {
-        return !database::blacklist.exists(id);
-    }
-}
 bool Authorized(const Message::Ptr &message, const int flags) {
     if (!gAuthorized || !isMessageUnderTimeLimit(message)) return false;
 
     if (message->from) {
-        return AuthorizedId(message->from->id, flags & AuthorizeFlags::PERMISSIVE);
+        const UserId id = message->from->id;
+        if (flags & AuthorizeFlags::PERMISSIVE) {
+            return !database::blacklist.exists(id);
+        } else {
+            if (database::whitelist.exists(id)) return true;
+            return id == database::db.maybeGetOwnerId();
+        }
     } else {
         return !(flags & AuthorizeFlags::REQUIRE_USER);
     }
