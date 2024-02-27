e9fbc47e704e43fb2626b646e6fc87b55f1b1134
roynatech2544 <whiteshell2544@naver.com>
2024-02-27 23:56:23 +0900

    TgBot++: Kill PRId64
    - No users

--- src/include/Types.h
+++ src/include/Types.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <cinttypes>
 #include <cstdint>
-
-// Helper for platform-independent 64bit int format
-#define LONGFMT "%" PRId64
 
 using UserId = std::int64_t;
 using ChatId = std::int64_t;
