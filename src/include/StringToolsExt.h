51d41ac169edf6c79d4aea304d664015ff68065b
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-28 15:42:27 +0900

    TgBot++: StringToolsExt: Fix when null terminator exists

--- src/include/StringToolsExt.h
+++ src/include/StringToolsExt.h
@@ -6,7 +6,7 @@
 
 static inline bool isEmptyOrBlank(const std::string& str) {
     return str.empty() || std::all_of(str.begin(), str.end(),
-                                      [](char c) { return std::isspace(c); });
+                                      [](char c) { return std::isspace(c) || c == 0; });
 }
 
 static inline void TrimStr(std::string& str) {
