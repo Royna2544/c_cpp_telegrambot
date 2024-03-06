b1166a6bd9fe5c85e5b169bc0186fe8e95494e63
roynatech2544 <whiteshell2544@naver.com>
2024-03-07 08:43:56 +0900

    TgBot++: Export TgBot::{Chat, User} helpers to header

---
+++ src/include/internal/_tgbot.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <tgbot/types/Chat.h>
+#include <tgbot/types/User.h>
+
+using TgBot::Chat;
+using TgBot::User;
+
+inline bool operator==(const Chat::Ptr thiz, const Chat::Ptr other) {
+    return thiz->id == other->id;
+}
+
+inline std::string UserPtr_toString(const User::Ptr bro) {
+    std::string username = bro->firstName;
+    if (!bro->lastName.empty())
+        username += ' ' + bro->lastName;
+    return '\'' + username + '\'';
+}
+
+inline std::string ChatPtr_toString(const Chat::Ptr ch) {
+    return '\'' + ch->title + '\'';
+}
