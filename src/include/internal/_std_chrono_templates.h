2d6b443e16abcd3df51b0f80532646f72ebdc1d2
roynatech2544 <whiteshell2544@naver.com>
2024-03-07 00:43:09 +0900

    TgBot++: Export std::chrono templates to header

---
+++ src/include/internal/_std_chrono_templates.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <chrono>
+
+using std::chrono_literals::operator""s;
+
+template <class Dur>
+std::chrono::hours to_hours(Dur &&it) {
+    return std::chrono::duration_cast<std::chrono::hours>(it);
+}
+
+template <class Dur>
+std::chrono::minutes to_mins(Dur &&it) {
+    return std::chrono::duration_cast<std::chrono::minutes>(it);
+}
+
+template <class Dur>
+std::chrono::seconds to_secs(Dur &&it) {
+    return std::chrono::duration_cast<std::chrono::seconds>(it);
+}
