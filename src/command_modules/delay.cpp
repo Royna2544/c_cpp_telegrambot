8ce4fcc3de2383f83539b20642afa1ce7d1c968f
roynatech2544 <whiteshell2544@naver.com>
2024-03-07 15:01:53 +0900

    TgBot++: Kill PrintableTime header

--- src/command_modules/delay.cpp
+++ src/command_modules/delay.cpp
@@ -1,20 +1,36 @@
 #include <chrono>
+#include <ctime>
 #include <ostream>
-#include <PrintableTime.h>
 #include <BotReplyMessage.h>
+#include <internal/_std_chrono_templates.h>
 
 #include "CommandModule.h"
+
+union Time_t {
+    time_t val;
+};
+
+std::ostream& operator<< (std::ostream& self, const Time_t tp) {
+    self << std::put_time(std::gmtime(&tp.val), "%Y-%m-%dT%H:%M:%SZ (GMT)");
+    return self;
+}
+
+template <class T>
+Time_t fromTP(std::chrono::time_point<T> it) {
+    return {std::chrono::system_clock::to_time_t(it)};
+}
 
 static void DelayCommandFn(const Bot &bot, const Message::Ptr message) {
     using std::chrono::duration;
     using std::chrono::high_resolution_clock;
-    union time now {
-        .val = time(nullptr)
-    }, msg{.val = message->date};
+    using std::chrono::system_clock;
     std::ostringstream ss;
+    auto msg = system_clock::from_time_t(message->date);
+    auto now = system_clock::now();
 
-    ss << "Request message sent at: " << msg << std::endl;
-    ss << "Received at: " << now << " Diff: " << now - msg << 's' << std::endl;
+    ss << "Request message sent at: " << Time_t{message->date} << std::endl;
+    ss << "Received at: " << fromTP(now) << std::endl;
+    ss << "Diff: " << to_secs(now - msg).count() << 's' << std::endl;
     auto beforeSend = high_resolution_clock::now();
     auto sentMsg = bot_sendReplyMessage(bot, message, ss.str());
     auto afterSend = high_resolution_clock::now();
