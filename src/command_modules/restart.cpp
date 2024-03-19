a03441fefece69f2893f7b1d37e57e0950d97f8e
roynatech2544 <whiteshell2544@naver.com>
2024-03-20 00:51:08 +0900

    TgBot++: restart cmd: Add a method to control restarts
    - Stop spamming restarts

--- src/command_modules/restart.cpp
+++ src/command_modules/restart.cpp
@@ -3,14 +3,33 @@
 
 #include "BotReplyMessage.h"
 #include "CommandModule.h"
+#include "Logging.h"
 
 extern char **environ;
 static void restartCommandFn(const Bot &bot, const Message::Ptr message) {
-    int argc;
-    char *const *argv;
+    int argc, count = 1;
+    char *const *argv, **myEnviron, restartBuf[32] = {0};
+
+    if (const auto var = getenv("RESTART"); var != nullptr) {
+        LOG(LogLevel::DEBUG, "RESTART env var set to %s", var);
+        unsetenv("RESTART");
+        if (std::stoi(var) == message->messageId) {
+            LOG(LogLevel::DEBUG, "Restart success! From messageId %d", message->messageId);
+            bot_sendReplyMessage(bot, message, "Restart success!");
+            return;
+        }
+    }
+    for (;environ[count]; ++count);
+    myEnviron = new char *[count + 2];
+    memcpy(myEnviron, environ, count * sizeof(char *));
+    snprintf(restartBuf, sizeof(restartBuf), "RESTART=%d", message->messageId);
+    myEnviron[count] = restartBuf;
+    myEnviron[count + 1] = 0;
     copyCommandLine(CommandLineOp::GET, &argc, &argv);
+    LOG(LogLevel::DEBUG, "Restarting bot with command line: %s, addenv %s",
+        argv[0], restartBuf);
     bot_sendReplyMessage(bot, message, "Restarting bot instance...");
-    execve(argv[0], argv, environ);
+    execve(argv[0], argv, myEnviron);
 }
 
 struct CommandModule cmd_restart("restart", "Restarts the bot",
