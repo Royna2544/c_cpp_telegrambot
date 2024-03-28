55d64dcabdc03354fef9c048fd8c01adab685067
roynatech2544 <whiteshell2544@naver.com>
2024-03-28 14:05:43 +0900

    TgBot++: tests: ResMgr: Fix tests

--- tests/ResourceManagerTest.cpp
+++ tests/ResourceManagerTest.cpp
@@ -1,25 +1,27 @@
 #include <gtest/gtest.h>
 
 #include <libos/libfs.hpp>
+
 #include "ResourceManager.h"
 
 class ResourceManagerTest : public testing::Test {
    protected:
     void SetUp() override { gResourceManager.preloadResourceDirectory(); }
     ResourceManager& gResourceManager = ResourceManager::getInstance();
+    constexpr static const char kResourceTestFile[] = "test.txt";
 };
 
 TEST_F(ResourceManagerTest, PreloadOneFile) {
     const std::string expected = "This is a test file";
     const std::string_view actual =
-        gResourceManager.getResource("test/test.txt");
+        gResourceManager.getResource(kResourceTestFile);
     EXPECT_EQ(expected, actual);
 }
 
 TEST_F(ResourceManagerTest, PreloadAgain) {
     bool rc = gResourceManager.preloadOneFile(
-        FS::getPathForType(FS::PathType::RESOURCES) / "test/test.txt");
+        FS::getPathForType(FS::PathType::RESOURCES) / kResourceTestFile);
     EXPECT_FALSE(rc);
-    rc = gResourceManager.preloadOneFile("test/test.txt");
+    rc = gResourceManager.preloadOneFile(kResourceTestFile);
     EXPECT_FALSE(rc);
 }
