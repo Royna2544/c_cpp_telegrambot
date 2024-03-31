5e1b823a726b24dbe1ca316e6dc31ab6cc1bd1e3
Soo-Hwan Na <whiteshell2544@naver.com>
2024-04-01 00:48:09 +0900

    TgBot++: spam cmd: Complete it

--- src/command_modules/spam.cpp
+++ src/command_modules/spam.cpp
@@ -7,17 +7,49 @@
 #include "tgbot/tools/StringTools.h"
 
 constexpr int MAX_SPAM_COUNT = 10;
+constexpr auto kSpamDelayTime = std::chrono::milliseconds(700);
 
-static void SpamCommandFn(const Bot &bot, const Message::Ptr message) {
+/**
+ * @brief Update the spam count, ensuring it does not exceed the maximum allowed
+ * count.
+ *
+ * @param count The current spam count.
+ */
+static void updateSpamCount(int& count) {
+    if (count > MAX_SPAM_COUNT) {
+        count = MAX_SPAM_COUNT;
+    }
+}
+
+static void SpamCommandFn(const Bot& bot, const Message::Ptr message) {
     if (message->replyToMessage) {
-        if (message->replyToMessage->sticker && hasExtArgs(message)) {
+        if (hasExtArgs(message)) {
             int count = stoi(parseExtArgs(message));
-            for (int _ = 0; _ < count; _++) {
-                bot_sendSticker(bot, message, message->replyToMessage->sticker);
-                std::this_thread::sleep_for(std::chrono::milliseconds(700));
+            updateSpamCount(count);
+            if (message->replyToMessage->sticker) {
+                for (int _ = 0; _ < count; _++) {
+                    bot_sendSticker(bot, message,
+                                    message->replyToMessage->sticker);
+                    std::this_thread::sleep_for(kSpamDelayTime);
+                }
+            } else if (message->replyToMessage->animation) {
+                for (int _ = 0; _ < count; _++) {
+                    bot.getApi().sendAnimation(
+                        message->chat->id,
+                        message->replyToMessage->animation->fileId);
+                    std::this_thread::sleep_for(kSpamDelayTime);
+                }
+            } else if (!message->replyToMessage->text.empty()) {
+                for (int _ = 0; _ < count; _++) {
+                    bot.getApi().sendMessage(message->chat->id,
+                                             message->replyToMessage->text);
+                    std::this_thread::sleep_for(kSpamDelayTime);
+                }
+            } else {
+                bot_sendReplyMessage(
+                    bot, message,
+                    "Supports sticker/GIF/text for reply to messages, give count");
             }
-        } else {
-            bot_sendReplyMessage(bot, message, "Supports sticker for reply to messages, give count");
         }
     } else if (hasExtArgs(message)) {
         std::string command = parseExtArgs(message);
@@ -25,9 +57,7 @@ static void SpamCommandFn(const Bot &bot, const Message::Ptr message) {
         if (const auto v = StringTools::split(command, ' '); v.size() >= 2) {
             spamData.first = std::stoi(v[0]);
             spamData.second = command.substr(command.find_first_of(' ') + 1);
-            if (spamData.first > MAX_SPAM_COUNT) {
-                spamData.first = MAX_SPAM_COUNT;
-            }
+            updateSpamCount(spamData.first);
             for (int _ = 0; _ < spamData.first; _++) {
                 bot_sendMessage(bot, message->chat->id, spamData.second);
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
