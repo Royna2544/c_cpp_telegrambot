9b3eb2857eefc05bffcaea46159f2a8629bd90a8
Soo-Hwan Na <whiteshell2544@naver.com>
2024-04-21 19:33:26 +0900

    TgBot++: Add DTState RTLoader module

---
+++ src/command_modules/runtime/cmd_DTState.cpp
@@ -0,0 +1,35 @@
+#include <BotReplyMessage.h>
+
+#include "cmd_dynamic.h"
+#include "command_modules/CommandModule.h"
+
+static void cmd_DT(const Bot& bot, const Message::Ptr& message) {
+    static std::once_flag once;
+    static MessageId msgid = 0;
+    static ChatId chatid = 0;
+    static bool ok = false;
+    std::call_once(once, [&bot, message]() {
+        auto msg =
+            bot_sendMessage(bot, message->from->id, "DT State: Available");
+        msgid = msg->messageId;
+        chatid = msg->chat->id;
+        ok = true;
+    });
+    switch (ok) {
+        case false:
+            bot_editMessage(bot, chatid, msgid, "DT State: Available");
+            break;
+        case true:
+            bot_editMessage(bot, chatid, msgid, "DT State: Unavailable");
+            break;
+    };
+    ok = !ok;
+}
+
+extern "C" {
+struct dynamicCommandModule DYN_COMMAND_SYM {
+    .mod = CommandModule("newdtstate", "Update DT state",
+                         CommandModule::Flags::Enforced, cmd_DT),
+    .isSupported = nullptr,
+};
+}
