74f1dd4e96f4499446428575b2c90909b80a70d9
Soo Hwan Na <whiteshell2544@naver.com>
2024-05-16 19:50:34 +0900

    TgBot++: restart cmd: Remove manual malloc

--- src/command_modules/restart.cpp
+++ src/command_modules/restart.cpp
@@ -11,10 +11,10 @@
 extern char **environ;
 static void restartCommandFn(const Bot &bot, const Message::Ptr message) {
     std::array<char, 32> restartBuf = {0};
-    char **myEnviron = nullptr;
     int argc = 0;
     int count = 1;
     char *const *argv = nullptr;
+    std::vector<char *> myEnviron;
 
     if (auto *const var = getenv("RESTART"); var != nullptr) {
         MessageId id;
@@ -30,22 +30,21 @@ static void restartCommandFn(const Bot &bot, const Message::Ptr message) {
         }
     }
     for (; environ[count]; ++count);
-    myEnviron = new char *[count + 2];
-    memcpy(myEnviron, environ, count * sizeof(char *));
+    myEnviron.reserve(count + 2);
+    memcpy(myEnviron.data(), environ, count * sizeof(char *));
     snprintf(restartBuf.data(), restartBuf.size(), "RESTART=%d",
              message->messageId);
     myEnviron[count] = restartBuf.data();
-    myEnviron[count + 1] = 0;
+    myEnviron[count + 1] = nullptr;
     copyCommandLine(CommandLineOp::GET, &argc, &argv);
     LOG(INFO) << "Restarting bot with exe: " << argv[0] << ", addenv "
               << restartBuf.data();
     bot_sendReplyMessage(bot, message, "Restarting bot instance...");
     defaultCleanupFunction();
-    execve(argv[0], argv, myEnviron);
-    delete[] myEnviron;
+    execve(argv[0], argv, myEnviron.data());
 }
 
-void loadcmd_restart(CommandModule& module) {
+void loadcmd_restart(CommandModule &module) {
     module.command = "restart";
     module.description = "Restarts the bot";
     module.flags = CommandModule::Flags::Enforced;
