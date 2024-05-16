811da9916197067dc9f9770fb3ccca12423c2ccf
Soo Hwan Na <whiteshell2544@naver.com>
2024-05-16 19:44:49 +0900

    TgBot++: TryParseStr: Copy try_parse str as ref

--- src/include/TryParseStr.hpp
+++ src/include/TryParseStr.hpp
@@ -5,18 +5,18 @@
 
 /**
  * @brief Parses the given string into the given output value.
- * 
+ *
  * @tparam T the type of the output value
  * @param str the string to parse
  * @param outval the output value to fill with the parsed value
  * @return true if the parsing was successful, false otherwise
  */
 template <typename T>
-requires std::is_trivially_copyable_v<T>
-bool try_parse(const std::string str, T* outval) {
+    requires std::is_trivially_copyable_v<T>
+bool try_parse(const std::string& str, T* outval) {
     std::stringstream ss(str);
     std::string unused;
-    T temp {};
+    T temp{};
 
     if (static_cast<bool>(ss >> temp)) {
         if (static_cast<bool>(ss >> unused)) {
