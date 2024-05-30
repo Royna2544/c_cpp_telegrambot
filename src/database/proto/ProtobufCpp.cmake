a40a5d3d708f3b67e9f6a298dbd3290db9b99f40
Soo Hwan Na <whiteshell2544@naver.com>
2024-05-30 21:23:05 +0900

    TgBot++: Kill proto/ directory
    - Move under src/database

--- src/database/proto/ProtobufCpp.cmake
+++ src/database/proto/ProtobufCpp.cmake
@@ -1,23 +1,9 @@
-function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
+function(MY_PROTOBUF_GENERATE_CPP SRCS HDRS)
   if(NOT ARGN)
     message(
       SEND_ERROR "Error: PROTOBUF_GENERATE_CPP() called without any proto files"
     )
     return()
-  endif()
-
-  if(PROTOBUF_GENERATE_CPP_APPEND_PATH)
-    # Create an include path for each file specified
-    foreach(FIL ${ARGN})
-      get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
-      get_filename_component(ABS_PATH ${ABS_FIL} PATH)
-      list(FIND _protobuf_include_path ${ABS_PATH} _contains_already)
-      if(${_contains_already} EQUAL -1)
-        list(APPEND _protobuf_include_path -I ${ABS_PATH})
-      endif()
-    endforeach()
-  else()
-    set(_protobuf_include_path -I ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
 
   if(DEFINED PROTOBUF_IMPORT_DIRS)
@@ -35,19 +21,20 @@ function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
   foreach(FIL ${ARGN})
     get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
     get_filename_component(FIL_WE ${FIL} NAME_WE)
+    get_filename_component(PATH_DIR ${FIL} DIRECTORY)
+    get_filename_component(PATH_FILE ${FIL} NAME)
 
-    set(PROTO_GENSRC_ROOT "${CMAKE_CURRENT_BINARY_DIR}/${PATH}")
-    set(PROTO_GENSRC_OUT "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/proto")
+    set(PROTO_GENSRC_ROOT "${CMAKE_CURRENT_BINARY_DIR}/${PATH_DIR}")
     set(PROTO_GEN_SRC "${FIL_WE}.pb.cc")
     set(PROTO_GEN_HDR "${FIL_WE}.pb.h")
-    list(APPEND ${SRCS} "${PROTO_GENSRC_OUT}/${PROTO_GEN_SRC}")
-    list(APPEND ${HDRS} "${PROTO_GENSRC_OUT}/${PROTO_GEN_HDR}")
+    list(APPEND ${SRCS} "${PROTO_GENSRC_ROOT}/${PROTO_GEN_SRC}")
+    list(APPEND ${HDRS} "${PROTO_GENSRC_ROOT}/${PROTO_GEN_HDR}")
 
     set(PROTO_GEN_SRCS "${PROTO_GENSRC_ROOT}/${PROTO_GEN_SRC}"
                        "${PROTO_GENSRC_ROOT}/${PROTO_GEN_HDR}")
+    set(PROTOC_EXE ${CMAKE_BINARY_DIR}/bin/protoc)
     execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory
                             ${PROTO_GENSRC_ROOT})
-
     add_custom_target(protobuf_${FIL_WE}_ready
       DEPENDS ${PROTO_GEN_SRCS}
       COMMENT "Generated ${FIL_WE} Proto Srcs..."
@@ -55,9 +42,9 @@ function(MY_PROTOBUF_GENERATE_CPP PATH SRCS HDRS)
     add_custom_command(
       OUTPUT ${PROTO_GEN_SRCS}
       COMMAND
-        protoc ARGS --cpp_out
-        ${CMAKE_CURRENT_BINARY_DIR}/${PATH} ${_protobuf_include_path} ${ABS_FIL}
-      DEPENDS ${ABS_FIL} protoc
+        ${CMAKE_COMMAND} -E chdir ${CMAKE_SOURCE_DIR}/${PATH_DIR} ${PROTOC_EXE} --cpp_out
+        ${CMAKE_CURRENT_BINARY_DIR}/${PATH_DIR} ${_protobuf_include_path} ${PATH_FILE}
+      DEPENDS ${ABS_FIL} ${PROTOC_EXE}
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
       VERBATIM)
   endforeach()
