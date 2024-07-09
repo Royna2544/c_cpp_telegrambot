60ee9c51e59d3fd38277b0082e40dc083befdaa3
Soo Hwan Na <whiteshell2544@naver.com>
2024-07-09 14:01:41 +0900

    TgBot++: Refactor TimerImpl to eject bot dependency

--- src/TimerImpl.cpp
+++ src/TimerImpl.cpp
@@ -1,188 +1,104 @@
-#include <BotReplyMessage.h>
-#include <TimerImpl.h>
 #include <absl/log/log.h>
 #include <internal/_std_chrono_templates.h>
 #include <internal/_tgbot.h>
 
 #include <ManagedThreads.hpp>
-#include <MessageWrapper.hpp>
+#include <TimerImpl.hpp>
 #include <chrono>
 #include <cmath>
+#include <sstream>
 
-bool TimerCommandManager::parseTimerArguments(const Bot &bot,
-                                              const Message::Ptr &message,
-                                              std::chrono::seconds &out) {
-    bool found = false;
-    bool result = false;
-    std::string msg;
-    MessageWrapper wrapper(bot, message);
-    enum {
-        HOUR,
-        MINUTE,
-        SECOND,
-        NONE,
-    } state = NONE;
+TimerThread::Result TimerThread::parse(const std::string &timeString) {
+    std::chrono::seconds parsedTimeLocal;
+    std::string token;
+    std::istringstream issTimeStr(timeString);
+    std::istringstream issSuffixStr;
 
-    if (wrapper.hasExtraText()) {
-        msg = wrapper.getExtraText();
-        found = true;
-    }
-    if (wrapper.hasExtraText()) {
-        wrapper.switchToReplyToMessage();
-        msg = wrapper.getText();
-        found = true;
-    }
-    if (!found) {
-        wrapper.sendMessageOnExit("Send or reply to a time, in hhmmss format");
-        return false;
-    }
     if (isactive) {
-        wrapper.sendMessageOnExit("Timer is already running");
-        return false;
+        return Result::TIMER_STATE_INVALID;
     }
-    const char *c_str = msg.c_str();
-    std::vector<int> numbercache;
-    for (size_t i = 0; i <= msg.size(); i++) {
-        int code = static_cast<int>(static_cast<unsigned char>(c_str[i]));
-        if (i == msg.size()) {
-            code = ' ';
+    parsedTime.reset();
+    while (std::getline(issTimeStr, token, ' ')) {
+        int num = 0;
+        char suffix{};
+
+        if (token.empty()) {
+            continue;
         }
-        switch (code) {
-            case ' ': {
-                if (!numbercache.empty()) {
-                    int result = 0, count = 1;
-                    for (const auto i : numbercache) {
-                        result += pow(10, numbercache.size() - count) * i;
-                        count++;
-                    }
-                    switch (state) {
-                        case HOUR:
-                            out += to_secs(std::chrono::hours(result));
-                            break;
-                        case MINUTE:
-                            out += to_secs(std::chrono::minutes(result));
-                            break;
-                        case SECOND:
-                            out += to_secs(std::chrono::seconds(result));
-                            break;
-                        default:
-                            break;
-                    }
-                    state = NONE;
-                    numbercache.clear();
-                }
-                break;
+
+        issSuffixStr = std::istringstream(token);
+        // Expect <number><h/m/s> format
+        if (issSuffixStr >> num && issSuffixStr >> suffix) {
+            DLOG(INFO) << "num: " << num << " suffix: " << suffix;
+            if (num < 0) {
+                LOG(WARNING) << "Negative number: " << num;
+                return Result::TIME_CANNOT_PARSE;
             }
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9': {
-                int intver = code - '0';
-                numbercache.push_back(intver);
-                break;
+            switch (suffix) {
+                case 'h':
+                    parsedTimeLocal += to_secs(std::chrono::hours(num));
+                    break;
+                case 'm':
+                    parsedTimeLocal += to_secs(std::chrono::minutes(num));
+                    break;
+                case 's':
+                    parsedTimeLocal += to_secs(std::chrono::seconds(num));
+                    break;
+                default:
+                    LOG(WARNING) << "Invalid suffix: " << suffix;
+                    return Result::TIME_CANNOT_PARSE;
             }
-            case 'H':
-            case 'h': {
-                state = HOUR;
-                break;
-            }
-            case 'M':
-            case 'm': {
-                state = MINUTE;
-                break;
-            }
-            case 'S':
-            case 's': {
-                state = SECOND;
-                break;
-            }
-            default: {
-                wrapper.sendMessageOnExit(
-                    "Invalid value provided.\nShould contain only h, m, s, "
-                    "numbers, spaces. (ex. 1h 20m 7s)");
-                return false;
-            }
+        } else {
+            LOG(WARNING) << "Invalid time format: " << std::quoted(token);
+            return Result::TIME_CANNOT_PARSE;
         }
     }
-    if (out.count() == 0) {
-        wrapper.sendMessageOnExit("I'm not a fool to time 0s");
-        return false;
-    } else if (out.count() < TIMER_CONFIG_SEC) {
-        wrapper.sendMessageOnExit("Provide longer time value");
-        return false;
-    } else if (to_hours(out).count() > 2) {
-        wrapper.sendMessageOnExit("Time provided is too long, which is: " +
-                                  to_string(out));
-        return false;
+    if (parsedTimeLocal < kMinTime) {
+        return Result::TIME_TOO_SHORT;
+    } else if (parsedTimeLocal > kMaxTime) {
+        return Result::TIME_TOO_LONG;
     }
-    return true;
+    parsedTime = parsedTimeLocal;
+    return Result::SUCCESS;
 }
 
-void TimerCommandManager::TimerThreadFn(const Bot &bot, Message::Ptr message,
-                                        std::chrono::seconds timer) {
+void TimerThread::runFunction() {
     isactive = true;
-    while (timer > 0s && kRun) {
+    onTimerStart();
+    while (timerLeftDuration > 0s && kRun) {
         std::this_thread::sleep_for(1s);
-        if (timer.count() % TIMER_CONFIG_SEC == 0) {
-            bot_editMessage(bot, message, to_string(timer));
+        if (timerLeftDuration % kUpdateDuration == 0s) {
+            onTimerPoint(timerLeftDuration);
         }
-        timer -= 1s;
+        timerLeftDuration -= 1s;
     }
-    bot_editMessage(bot, message, "Timer ended");
-    if (sendendmsg) {
-        bot_sendMessage(bot, message->chat->id, "Timer ended");
-    }
-    if (botcanpin) {
-        bot.getApi().unpinChatMessage(message->chat->id, message->messageId);
-    }
+    onTimerEnd();
     isactive = false;
 }
 
-void TimerCommandManager::startTimer(const Bot &bot, const Message::Ptr &msg) {
-    std::chrono::seconds parsedTime(0);
-
-    if (parseTimerArguments(bot, msg, parsedTime)) {
-        message = bot_sendMessage(bot, msg->chat->id,
-                                  "Timer starting: " + to_string(parsedTime));
-        botcanpin = true;
-        try {
-            bot.getApi().pinChatMessage(message->chat->id, message->messageId);
-        } catch (const TgBot::TgException &) {
-            LOG(WARNING) << "Cannot pin msg!";
-            botcanpin = false;
-        }
-        setPreStopFunction(TimerCommandManager::Timerstop);
-        runWith([this, capture0 = std::cref(bot), parsedTime] {
-            TimerThreadFn(capture0, message, parsedTime);
-        });
+std::optional<std::string> TimerThread::describeParsedTime() const {
+    if (!parsedTime) {
+        return std::nullopt;
     }
+    return to_string(parsedTime.value());
 }
 
-void TimerCommandManager::stopTimer(const Bot &bot, const Message::Ptr &msg) {
-    std::string text;
+TimerThread::Result TimerThread::start() {
     if (isactive) {
-        const bool allowed = message->chat == msg->chat;
-        sendendmsg = !allowed;
-        if (allowed) {
-            stop();
-            text = "Stopped successfully";
-        } else {
-            text = "Timer is running on other group.";
-        }
-    } else {
-        text = "Timer is not running";
+        return Result::TIMER_STATE_INVALID;
     }
-    bot_sendReplyMessage(bot, msg, text);
+    if (!parsedTime) {
+        return Result::TIMER_NOT_SET;
+    }
+    timerLeftDuration = parsedTime.value();
+    run();
+    return Result::SUCCESS;
 }
 
-void TimerCommandManager::Timerstop(ManagedThread *thiz) {
-    if (dynamic_cast<TimerCommandManager *>(thiz)->isactive) {
-        LOG(INFO) << "Canceling timer and cleaning up...";
+TimerThread::Result TimerThread::stop() {
+    if (!isactive) {
+        return Result::TIMER_STATE_INVALID;
     }
+    ManagedThreadRunnable::stop();
+    return Result::SUCCESS;
 }
--- src/command_modules/timer_impl.cpp
+++ src/command_modules/timer_impl.cpp
@@ -1,33 +1,93 @@
-#include <TimerImpl.h>
-
 #include <ManagedThreads.hpp>
+#include <TimerImpl.hpp>
+#include <memory>
 
+#include "BotReplyMessage.h"
 #include "CommandModule.h"
+#include "TgBotWrapper.hpp"
+
+struct TimerImplThread : TimerThread {
+    void onTimerStart() override {
+        message = botwrapper->sendMessage(chatid, "Timer started");
+    }
+    void onTimerPoint(const std::chrono::seconds &timeLeft) override {
+        message = botwrapper->editMessage(message,
+                                          "Time left: " + to_string(timeLeft));
+    }
+    void onTimerEnd() override {
+        message = botwrapper->editMessage(message, "Timer ended");
+    }
+    explicit TimerImplThread(ChatId chatid) : chatid(chatid) {}
+
+   private:
+    std::shared_ptr<TgBotWrapper> botwrapper = TgBotWrapper::getInstance();
+    Message::Ptr message;
+    ChatId chatid;
+};
 
 static void TimerStartCommandFn(const Bot &bot, const Message::Ptr message) {
     const auto tm = ThreadManager::getInstance();
     auto ctrl = tm->createController<ThreadManager::Usage::TIMER_THREAD,
-                                     TimerCommandManager>();
-    if (ctrl) {
-        ctrl->startTimer(bot, message);
-    } else {
-        ctrl = tm->getController<ThreadManager::Usage::TIMER_THREAD, TimerCommandManager>();
-        CHECK(ctrl) << "Timer should be gettable";
-        ctrl->startTimer(bot, message);
+
+                                     TimerImplThread>(message->chat->id);
+    MessageWrapper wrapper(message);
+    bool ok = false;
+    std::string result;
+    if (wrapper.hasExtraText()) {
+        switch (ctrl->parse(wrapper.getExtraText())) {
+            case TimerThread::Result::TIME_CANNOT_PARSE:
+                result = "Could not parse time: " + wrapper.getExtraText();
+                break;
+            case TimerThread::Result::TIME_TOO_SHORT:
+                result = "Time too short. Please specify a time longer";
+                break;
+            case TimerThread::Result::TIME_TOO_LONG:
+                result = "Time too long. Please specify a time shorter";
+                break;
+            case TimerThread::Result::SUCCESS:
+                result = "Parsed as " + ctrl->describeParsedTime().value();
+                ok = true;
+                break;
+            default:
+                CHECK(false) << "Invalid timer result";
+                break;
+        }
+    }
+    if (ctrl && ok) {
+        ok = false;
+        switch (ctrl->start()) {
+            case TimerThread::Result::TIMER_STATE_INVALID:
+                result = "Timer is already running";
+                break;
+            case TimerThread::Result::TIMER_NOT_SET:
+                result = "Timer is not set";
+                break;
+            case TimerThread::Result::SUCCESS:
+                ok = true;
+                ctrl->start();
+                break;
+            default:
+                CHECK(false) << "Invalid timer state";
+                break;
+        }
+    }
+    if (!ok) {
+        tm->destroyController(ThreadManager::Usage::TIMER_THREAD);
+    }
+    if (!result.empty()) {
+        bot_sendReplyMessage(bot, message, result);
     }
 }
 
-static void TimerStopCommandFn(const Bot &bot, const Message::Ptr message) {
+static void TimerStopCommandFn(const Bot &bot, const Message::Ptr& message) {
     const auto tm = ThreadManager::getInstance();
     auto ctrl = tm->getController<ThreadManager::Usage::TIMER_THREAD,
-                                     TimerCommandManager>();
+                                  TimerImplThread>();
     if (ctrl) {
-        ctrl->stopTimer(bot, message);
-    } else {
-        ctrl = tm->createController<ThreadManager::Usage::TIMER_THREAD, TimerCommandManager>();
-        CHECK(ctrl) << "Timer should be gettable";
-        ctrl->stopTimer(bot, message);
+        ctrl->stop();
         tm->destroyController(ThreadManager::Usage::TIMER_THREAD);
+    } else {
+        bot_sendReplyMessage(bot, message, "Timer not started");
     }
 }
 
