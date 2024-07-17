ffea354fca09832317de8168f076a27a48c314b3
Soo Hwan Na <whiteshell2544@naver.com>
2024-07-17 13:22:48 +0900

    TgBot++: tests: Authorization: Move DB load/unload to itself
    - Not the main()

--- tests/AuthorizationTest.cpp
+++ tests/AuthorizationTest.cpp
@@ -6,100 +6,115 @@
 
 #include "database/bot/TgBotDatabaseImpl.hpp"
 
-static void MakeMessageDateNow(Message::Ptr& message) {
-    const auto rn = std::chrono::system_clock::now();
-    message->date = std::chrono::system_clock::to_time_t(rn);
-}
+class AuthorizationTest : public ::testing::Test {
+   protected:
+    static void SetUpTestSuite() {
+        const auto dbImpl = TgBotDatabaseImpl::getInstance();
+        ASSERT_TRUE(dbImpl->loadDBFromConfig());
+    }
 
-static void MakeMessageDateBefore(Message::Ptr& message) {
-    using std::chrono_literals::operator""s;
+    static void TearDownTestSuite() {
+        const auto dbImpl = TgBotDatabaseImpl::getInstance();
+        ASSERT_TRUE(dbImpl->unloadDatabase());
+    }
 
-    const auto before =
-        std::chrono::system_clock::now() - kMaxTimestampDelay - 1s;
-    message->date = std::chrono::system_clock::to_time_t(before);
-}
+   public:
+    static void MakeMessageDateNow(Message::Ptr& message) {
+        const auto rn = std::chrono::system_clock::now();
+        message->date = std::chrono::system_clock::to_time_t(rn);
+    }
 
-static void MakeMessageNonOwner(Message::Ptr& message) {
-    message->from = std::make_shared<TgBot::User>();
-}
+    static void MakeMessageDateBefore(Message::Ptr& message) {
+        using std::chrono_literals::operator""s;
 
-static void MakeMessageOwner(Message::Ptr& message) {
-    static UserId ownerId = TgBotDatabaseImpl::getInstance()->getOwnerUserId().value_or(12345);
-    MakeMessageNonOwner(message);
-    message->from->id = ownerId;
-}
-static bool Authorized(const Message::Ptr& message, unsigned flags) {
-    static auto authflags = AuthContext::getInstance();
-    return authflags->isAuthorized(message, flags);
-}
+        const auto before =
+            std::chrono::system_clock::now() - kMaxTimestampDelay - 1s;
+        message->date = std::chrono::system_clock::to_time_t(before);
+    }
 
-TEST(AuthorizationTest, TimeNowOwnerEnforce) {
+    static void MakeMessageNonOwner(Message::Ptr& message) {
+        message->from = std::make_shared<TgBot::User>();
+    }
+
+    static void MakeMessageOwner(Message::Ptr& message) {
+        static UserId ownerId =
+            TgBotDatabaseImpl::getInstance()->getOwnerUserId().value_or(12345);
+        MakeMessageNonOwner(message);
+        message->from->id = ownerId;
+    }
+    static bool Authorized(const Message::Ptr& message, unsigned flags) {
+        static auto authflags = AuthContext::getInstance();
+        return authflags->isAuthorized(message, flags);
+    }
+};
+
+TEST_F(AuthorizationTest, TimeNowOwnerEnforce) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
     MakeMessageOwner(dummyMsg);
     ASSERT_TRUE(Authorized(dummyMsg, 0));
 }
 
-TEST(AuthorizationTest, TimeBeforeOwnerEnforce) {
+TEST_F(AuthorizationTest, TimeBeforeOwnerEnforce) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateBefore(dummyMsg);
     MakeMessageOwner(dummyMsg);
     ASSERT_FALSE(Authorized(dummyMsg, 0));
 }
 
-TEST(AuthorizationTest, TimeNowNonOwnerEnforce) {
+TEST_F(AuthorizationTest, TimeNowNonOwnerEnforce) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
     MakeMessageNonOwner(dummyMsg);
     ASSERT_FALSE(Authorized(dummyMsg, 0));
 }
 
-TEST(AuthorizationTest, TimeBeforeNonOwnerEnforce) {
+TEST_F(AuthorizationTest, TimeBeforeNonOwnerEnforce) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateBefore(dummyMsg);
     MakeMessageNonOwner(dummyMsg);
     ASSERT_FALSE(Authorized(dummyMsg, 0));
 }
 
-TEST(AuthorizationTest, TimeNowOwnerPermissive) {
+TEST_F(AuthorizationTest, TimeNowOwnerPermissive) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
     MakeMessageOwner(dummyMsg);
     ASSERT_TRUE(Authorized(dummyMsg, AuthContext::Flags::PERMISSIVE));
 }
 
-TEST(AuthorizationTest, TimeBeforeOwnerPermissive) {
+TEST_F(AuthorizationTest, TimeBeforeOwnerPermissive) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateBefore(dummyMsg);
     MakeMessageOwner(dummyMsg);
     ASSERT_FALSE(Authorized(dummyMsg, AuthContext::Flags::PERMISSIVE));
 }
 
-TEST(AuthorizationTest, TimeNowNonOwnerPermissive) {
+TEST_F(AuthorizationTest, TimeNowNonOwnerPermissive) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
     MakeMessageNonOwner(dummyMsg);
     ASSERT_TRUE(Authorized(dummyMsg, AuthContext::Flags::PERMISSIVE));
 }
 
-TEST(AuthorizationTest, TimeBeforeNonOwnerPermissive) {
+TEST_F(AuthorizationTest, TimeBeforeNonOwnerPermissive) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateBefore(dummyMsg);
     MakeMessageNonOwner(dummyMsg);
     ASSERT_FALSE(Authorized(dummyMsg, AuthContext::Flags::PERMISSIVE));
 }
 
-TEST(AuthorizationTest, UserRequiredNoUser) {
+TEST_F(AuthorizationTest, UserRequiredNoUser) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
-    ASSERT_FALSE(Authorized(
-        dummyMsg, AuthContext::Flags::REQUIRE_USER | AuthContext::Flags::PERMISSIVE));
+    ASSERT_FALSE(Authorized(dummyMsg, AuthContext::Flags::REQUIRE_USER |
+                                          AuthContext::Flags::PERMISSIVE));
 }
 
-TEST(AuthorizationTest, UserRequiredUser) {
+TEST_F(AuthorizationTest, UserRequiredUser) {
     auto dummyMsg = std::make_shared<Message>();
     MakeMessageDateNow(dummyMsg);
     MakeMessageNonOwner(dummyMsg);
-    ASSERT_TRUE(Authorized(
-        dummyMsg, AuthContext::Flags::REQUIRE_USER | AuthContext::Flags::PERMISSIVE));
+    ASSERT_TRUE(Authorized(dummyMsg, AuthContext::Flags::REQUIRE_USER |
+                                         AuthContext::Flags::PERMISSIVE));
 }
