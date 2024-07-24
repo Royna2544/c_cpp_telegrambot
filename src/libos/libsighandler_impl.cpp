b19b40ec98ea60f92ee125476893ab8811cfc54c
roynatech2544 <whiteshell2544@naver.com>
2024-07-24 12:24:34 +0900

    sighandler: Print exiting message only once

--- src/libos/libsighandler_impl.cpp
+++ src/libos/libsighandler_impl.cpp
@@ -1,12 +1,15 @@
+#include <absl/log/log.h>
+
 #include "OnTerminateRegistrar.hpp"
 #include "libsighandler.hpp"
-#include <absl/log/log.h>
 
 std::atomic_bool SignalHandler::kUnderSignal;
 
 void SignalHandler::signalHandler() {
-    LOG(INFO) << "Received signal...";
-    kUnderSignal = true;
+    if (!kUnderSignal) {
+        LOG(INFO) << "Received signal...";
+        kUnderSignal = true;
+    }
 }
 
 DECLARE_CLASS_INST(OnTerminateRegistrar);
