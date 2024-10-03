484ec953547d9122ca6d8b3e87c1a337e6c0c96a
royna2544 <whiteshell2544@naver.com>
2024-10-03 19:19:21 +0900

    Authorization: Remove debug

--- src/Authorization.cpp
+++ src/Authorization.cpp
@@ -1,18 +1,10 @@
 #include <Authorization.h>
 #include <Types.h>
+#include <absl/log/log.h>
 
 #include <InstanceClassBase.hpp>
 #include <database/DatabaseBase.hpp>
-
-#include "absl/log/log.h"
-#include "database/bot/TgBotDatabaseImpl.hpp"
-
-// #define AUTHORIZATION_DEBUG
-
-#ifdef AUTHORIZATION_DEBUG
-#include <iomanip>
-#include <mutex>
-#endif
+#include <database/bot/TgBotDatabaseImpl.hpp>
 
 DECLARE_CLASS_INST(AuthContext);
 
@@ -37,21 +29,7 @@ bool isInList(const UserId user) {
 AuthContext::Result AuthContext::isAuthorized(const Message::Ptr& message,
                                               const unsigned flags) const {
     const auto database = TgBotDatabaseImpl::getInstance();
-#ifdef AUTHORIZATION_DEBUG
-    static std::mutex authStdoutLock;
-#endif
     if (!authorized || !isMessageUnderTimeLimit(message)) {
-#ifdef AUTHORIZATION_DEBUG
-        const std::lock_guard<std::mutex> _(authStdoutLock);
-        DLOG(INFO) << "Not authorized: message text: "
-                   << std::quoted(message->text);
-        if (!authorized) {
-            DLOG(INFO) << "why?: Global authorization flag is off";
-        }
-        if (!isMessageUnderTimeLimit(message)) {
-            DLOG(INFO) << "why?: Message is too old";
-        }
-#endif
         Result::Reason reason{};
         if (!authorized) {
             reason = Result::Reason::GLOBAL_FLAG_OFF;
@@ -66,35 +44,15 @@ AuthContext::Result AuthContext::isAuthorized(const Message::Ptr& message,
         const UserId id = message->from->id;
         if ((flags & Flags::PERMISSIVE) != 0) {
             bool isInBlacklist = false;
-#ifdef AUTHORIZATION_DEBUG
-            const std::lock_guard<std::mutex> _(authStdoutLock);
-
-            DLOG(INFO) << "Checking if user id in blacklist";
-#endif
             isInBlacklist = isInList<DatabaseBase::ListType::BLACKLIST>(id);
-#ifdef AUTHORIZATION_DEBUG
-            DLOG(INFO) << "User id in blacklist: " << std::boolalpha
-                       << isInBlacklist;
-#endif
             return {!isInBlacklist, Result::Reason::BLACKLISTED_USER};
         } else {
             bool ret = isInList<DatabaseBase::ListType::WHITELIST>(id);
             bool ret2 = id == database->getOwnerUserId();
-#ifdef AUTHORIZATION_DEBUG
-            const std::lock_guard<std::mutex> _(authStdoutLock);
-
-            DLOG(INFO) << "Checking if user id in whitelist: " << std::boolalpha
-                       << ret;
-            DLOG(INFO) << "User is owner of this bot: " << std::boolalpha
-                       << ret2;
-#endif
             return {ret || ret2, Result::Reason::NOT_IN_WHITELIST};
         }
     } else {
         bool ignore = (flags & Flags::REQUIRE_USER) == Flags::REQUIRE_USER;
-#ifdef AUTHORIZATION_DEBUG
-        DLOG(INFO) << "Should ignore the message: " << std::boolalpha << ignore;
-#endif
         return {!ignore, Result::Reason::REQUIRES_USER};
     }
 }
