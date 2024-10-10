2596c0d98a5b782689993e40f5adbf614a65451b
royna2544 <whiteshell2544@naver.com>
2024-10-10 22:42:45 +0900

    Install sqlite on CI

--- resources/scripts/setup_dependencies_apt.sh
+++ resources/scripts/setup_dependencies_apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APT_COMMAND="apt update; apt install libcurl4-openssl-dev libssl-dev libboost-all-dev \
-    cmake clang ninja-build cargo libpng-dev libwebp-dev \
+    cmake clang ninja-build cargo libpng-dev libwebp-dev libsqlite3-dev \
     libjpeg-dev libopencv-dev libgit2-dev libgtest-dev libgmock-dev libjsoncpp-dev libfmt-dev"
 
 function lecho () {
