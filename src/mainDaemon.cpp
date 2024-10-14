63d8317969c6d2d688a90c16460f27e39e29df28
royna2544 <whiteshell2544@naver.com>
2024-10-14 15:47:08 +0900

    mainDaemon: Use absl logging, make it launch loop

--- src/mainDaemon.cpp
+++ src/mainDaemon.cpp
@@ -1,94 +1,134 @@
+#include <absl/log/log.h>
+#include <fcntl.h>
+#include <fmt/chrono.h>
 #include <fmt/format.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
+#include <StructF.hpp>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <limits>
+#include <logging/AbslLogInit.hpp>
+#include <string_view>
 
 // Wrapper launcher to create a daemon process from the bot
 int main(const int argc, char** argv) {
-    const auto kLogFile = fmt::format("log_{}.txt", getpid());
+    const std::filesystem::path kLogFile = fmt::format(
+        "log_{:%Y_%m_%d_%H_%M_%S}.txt", std::chrono::system_clock::now());
+    TgBot_AbslLogInit();
 
     if (argc < 2) {
-        puts("A wrapper launcher to create a daemon process.");
-        puts("No bot executable provided.");
-        fmt::print("Usage: {} <bot_exe_path> args...\n", argv[0]);
+        LOG(INFO) << "A wrapper launcher to create a daemon process.";
+        LOG(INFO) << "No bot executable provided.";
+        LOG(INFO) << fmt::format("Usage: {} <bot_exe_path> args...", argv[0]);
         return EXIT_FAILURE;
     }
-    fmt::print("My pid is {}\n", getpid());
 
+    if (access(argv[1], R_OK | X_OK) != 0) {
+        PLOG(ERROR) << "Bot executable not found or not executable.";
+        return EXIT_FAILURE;
+    }
+
+    LOG(INFO) << fmt::format("Parent pid is {}", getpid());
     pid_t pid = fork();
 
     if (pid < 0) {
-        perror("Fork failed");
+        PLOG(ERROR) << "Fork failed";
         return EXIT_FAILURE;
     }
 
     if (pid > 0) {
         exit(EXIT_SUCCESS);  // Parent exits
-    }
-
-    // Child process continues
-    fmt::print("Daemon process created with PID: {}\n", getpid());
-    fmt::print("Redirecting stdout and stderr to {}\n", kLogFile.data());
-    fmt::print("Executable is: {}, argument count: {}\n", argv[1], argc - 1);
-
-    if (access(argv[1], R_OK | X_OK) != 0) {
-        std::cerr << "Error: Bot executable not found or not executable."
-                  << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Create a new session
-    if (setsid() < 0) {
-        perror("setsid failed");
-        return EXIT_FAILURE;
-    }
-
-    // Optional: Second fork to prevent the daemon from acquiring a terminal
-    pid = fork();
-    if (pid < 0) {
-        perror("Second fork failed");
-        return EXIT_FAILURE;
-    }
-
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);  // Parent exits
-    }
-
-    // Skip changing working directory to root directory
-
-    // Close all open file descriptors
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-
-    if (access(kLogFile.data(), F_OK) == 0) {
-        puts("Removing old log file");
-        unlink(kLogFile.data());  // Remove old log file
     }
 
     // Redirect file descriptors to /dev/null and bot.log
-    if (!freopen("/dev/null", "r", stdin)) {
-        perror("freopen stdin failed");
+    int devnull_fd = open("/dev/null", O_RDONLY);
+    if (devnull_fd < 0) {
+        PLOG(ERROR) << "Open /dev/null failed";
         return EXIT_FAILURE;
     }
-    if (!freopen(kLogFile.data(), "a+", stdout)) {
-        perror("freopen stdout failed");
-        return EXIT_FAILURE;
+    int logFile_fd = open(kLogFile.c_str(), O_RDONLY);
+    if (logFile_fd > 0) {
+        close(logFile_fd);
+        // This means the file already exists.
+        std::filesystem::remove(kLogFile);
     }
-    if (!freopen(kLogFile.data(), "a+", stderr)) {
-        perror("freopen stderr failed");
+    logFile_fd = open(kLogFile.c_str(), O_CREAT | O_WRONLY, 0644);
+    if (logFile_fd < 0) {
+        PLOG(ERROR) << "Open bot.log failed";
         return EXIT_FAILURE;
     }
 
-    // Set the process group ID to the same as the process ID
-    setpgid(0, 0);
+    dup2(devnull_fd, STDIN_FILENO);
+    dup2(logFile_fd, STDOUT_FILENO);
+    dup2(logFile_fd, STDERR_FILENO);
 
-    // Run the bot in the background
-    // Increment argv to skip the first element
-    argv++;
-    return execvp(argv[0], argv);  // Pass the modified argv to execvp
+    // Child process continues
+    LOG(INFO) << fmt::format("Daemon process created with PID: {}", getpid());
+    LOG(INFO) << fmt::format("Redirecting stdout and stderr to {}",
+                             kLogFile.c_str());
+    LOG(INFO) << fmt::format("Executable is: {}, argument count: {}", argv[1],
+                             argc - 1);
+
+    // Create a new session
+    if (setsid() < 0) {
+        PLOG(ERROR) << "setsid failed";
+        return EXIT_FAILURE;
+    }
+redo_vfork:
+    // Second fork to prevent the daemon from acquiring a terminal
+    pid = vfork();
+    if (pid < 0) {
+        PLOG(ERROR) << "Second vfork failed";
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0) {
+        // Set the process group ID to the same as the process ID
+        setpgrp();
+        // Increment argv to skip the first element
+        argv++;
+        // Execute the bot executable with the provided arguments
+        LOG(INFO) << "Executing the child process, " << argv[0] << " with pid " << getpid();
+        execvp(argv[0], argv);  // Pass the modified argv to execvp
+        _exit(std::numeric_limits<std::uint8_t>::max());
+    } else {
+        F pidFile;
+        constexpr std::string_view kPidFile = "bot.pid";
+        if (!pidFile.open(kPidFile, F::Mode::Write)) {
+            PLOG(ERROR) << "Failed to open bot.pid";
+            return EXIT_FAILURE;
+        }
+        if (pidFile.puts(fmt::format("{}", pid)) != F::Result::ok()) {
+            LOG(ERROR) << "Failed to write to bot.pid";
+            return EXIT_FAILURE;
+        }
+        pidFile.close();
+
+        int status{};
+        if (waitpid(pid, &status, 0) < 0) {
+            PLOG(ERROR) << "Failed to wait for child";
+            return EXIT_FAILURE;
+        } else {
+            if (WIFEXITED(status)) {
+                LOG(INFO) << fmt::format("Child process exited with status: {}",
+                                         WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                LOG(INFO) << fmt::format(
+                    "Child process terminated by signal: {}", WTERMSIG(status));
+            }
+        }
+        LOG(INFO) << "Consumed child death";
+        if (!std::filesystem::exists(kPidFile)) {
+            LOG(INFO) << "Pid file gone, exiting";
+            return EXIT_SUCCESS;
+        }
+        goto redo_vfork;  // Restart the daemon process loop
+    }
 }
