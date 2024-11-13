4a49484cb3c10d00fd5782f72d68604bd52b6ac8
royna2544 <whiteshell2544@naver.com>
2024-11-13 14:57:16 +0900

    ChatDataCollector: Move to proper cpp file

---
+++ src/ml/ChatDataCollector.cpp
@@ -0,0 +1,63 @@
+#include "ChatDataCollector.hpp"
+
+#include <absl/log/log.h>
+
+#include <filesystem>
+#include <fstream>
+
+ChatDataCollector::Data::Data(const Message::Ptr& message) {
+    if (!message->text.empty()) {
+        msgType = Data::MsgType::TEXT;
+    } else if (!message->photo.empty()) {
+        msgType = Data::MsgType::PHOTO;
+    } else if (message->video) {
+        msgType = Data::MsgType::VIDEO;
+    } else if (message->document) {
+        msgType = Data::MsgType::DOCUMENT;
+    } else if (message->sticker) {
+        msgType = Data::MsgType::STICKER;
+    } else if (message->animation) {
+        msgType = Data::MsgType::GIF;
+    } else {
+        msgType = Data::MsgType::ETC;
+    }
+    chatId = message->chat->id;
+    userId = message->from->id;
+    timestamp = message->date;
+}
+
+ChatDataCollector::ChatDataCollector(TgBotApi::Ptr api) {
+    api->onAnyMessage([this](TgBotApi::CPtr api, const Message::Ptr& message) {
+        onMessage(message);
+        return TgBotApi::AnyMessageResult::Handled;
+    });
+}
+
+ChatDataCollector::~ChatDataCollector() {
+    bool existed = false;
+    constexpr std::string_view kChatDataFile = "chat_data.csv";
+
+    if (chatData.empty()) {
+        LOG(INFO) << "No chat data collected, skipping chat data writing";
+        return;
+    }
+    // Write chat data to chat_data.csv
+    if (std::filesystem::exists(kChatDataFile)) {
+        // Then skip writing header
+        existed = true;
+    }
+    std::ofstream chatDataFile;
+    if (existed) {
+        chatDataFile.open(kChatDataFile.data(), std::ios::app);
+    } else {
+        chatDataFile.open(kChatDataFile.data());
+    }
+    if (!existed) {
+        chatDataFile << "chat_id,user_id,timestamp,message_type\n";
+    }
+    for (const auto& data : chatData) {
+        chatDataFile << data;
+    }
+    LOG(INFO) << "Chat data collected and saved to chat_data.csv: "
+              << chatData.size() << " entries";
+}
--- src/ml/ChatDataCollector.hpp
+++ src/ml/ChatDataCollector.hpp
@@ -1,10 +1,8 @@
 #include <Types.h>
 
-#include <api/TgBotApiImpl.hpp>
+#include <api/TgBotApi.hpp>
 #include <ctime>
-#include <fstream>
-#include "api/TgBotApi.hpp"
-#include "trivial_helpers/fruit_inject.hpp"
+#include <trivial_helpers/fruit_inject.hpp>
 
 // Collects user data to a CSV
 class ChatDataCollector {
@@ -24,26 +22,7 @@ class ChatDataCollector {
         } msgType{};
 
         Data() = default;
-        explicit Data(const Message::Ptr& message) {
-            if (!message->text.empty()) {
-                msgType = Data::MsgType::TEXT;
-            } else if (!message->photo.empty()) {
-                msgType = Data::MsgType::PHOTO;
-            } else if (message->video) {
-                msgType = Data::MsgType::VIDEO;
-            } else if (message->document) {
-                msgType = Data::MsgType::DOCUMENT;
-            } else if (message->sticker) {
-                msgType = Data::MsgType::STICKER;
-            } else if (message->animation) {
-                msgType = Data::MsgType::GIF;
-            } else {
-                msgType = Data::MsgType::ETC;
-            }
-            chatId = message->chat->id;
-            userId = message->from->id;
-            timestamp = message->date;
-        }
+        explicit Data(const Message::Ptr& message);
     };
 
     void onMessage(const Message::Ptr& message) {
@@ -61,41 +40,4 @@ inline std::ostream& operator<<(std::ostream& os, ChatDataCollector::Data d) {
     os << d.chatId << "," << d.userId << "," << d.timestamp << ","
        << static_cast<int>(d.msgType) << "\n";
     return os;
-}
-
-
-inline ChatDataCollector::ChatDataCollector(TgBotApi::Ptr api) {
-    api->onAnyMessage([this](TgBotApi::CPtr api, const Message::Ptr& message) {
-        onMessage(message);
-        return TgBotApiImpl::AnyMessageResult::Handled;
-    });
-}
-
-inline ChatDataCollector::~ChatDataCollector() {
-    bool existed = false;
-    constexpr const char* kChatDataFile = "chat_data.csv";
-
-    if (chatData.empty()) {
-        LOG(INFO) << "No chat data collected, skipping chat data writing";
-        return;
-    }
-    // Write chat data to chat_data.csv
-    if (std::filesystem::exists(kChatDataFile)) {
-        // Then skip writing header
-        existed = true;
-    }
-    std::ofstream chatDataFile;
-    if (existed) {
-        chatDataFile.open(kChatDataFile, std::ios::app);
-    } else {
-        chatDataFile.open(kChatDataFile);
-    }
-    if (!existed) {
-        chatDataFile << "chat_id,user_id,timestamp,message_type\n";
-    }
-    for (const auto& data : chatData) {
-        chatDataFile << data;
-    }
-    LOG(INFO) << "Chat data collected and saved to chat_data.csv: "
-              << chatData.size() << " entries";
 }
