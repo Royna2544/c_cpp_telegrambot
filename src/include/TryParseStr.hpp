50be3f918c54b0f93c7043c436a4b56a56c5f315
royna2544 <whiteshell2544@naver.com>
2024-11-17 14:16:25 +0900

    TryParseStr: Fix compability with MSVC
    - Where absl::string_view isnt std::string_view

--- src/include/TryParseStr.hpp
+++ src/include/TryParseStr.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <absl/strings/string_view.h>
+
 #include <sstream>
 #include <type_traits>
+
+namespace details::string {
 
 /**
  * @brief Parses the given string into the given output value.
@@ -11,9 +15,10 @@
  * @param outval the output value to fill with the parsed value
  * @return true if the parsing was successful, false otherwise
  */
-template <typename String, typename StringView, typename StringStream, typename T>
+template <typename String, typename StringView, typename StringStream,
+          typename T>
     requires std::is_trivially_copyable_v<T>
-bool _try_parse(const StringView& str, T* outval) {
+bool try_parse(const StringView& str, T* outval) {
     StringStream ss(str.data());
     String unused;
     T temp{};
@@ -32,17 +37,44 @@ bool _try_parse(const StringView& str, T* outval) {
 }
 
 template <typename String, typename StringView, typename StringStream>
-bool _try_parse(const String& str, String* outval) {
+bool try_parse(const String& str, String* outval) {
     *outval = str;
     return true;
 }
 
+}  // namespace details::string
+
 template <typename T>
 bool try_parse(const std::string_view str, T* outval) {
-    return _try_parse<std::string, std::string_view, std::stringstream>(str, outval);
+    return details::string::try_parse<std::string, std::string_view,
+                                      std::stringstream>(str, outval);
 }
 
 template <typename T>
 bool try_parse(const std::wstring_view str, T* outval) {
-    return _try_parse<std::wstring, std::wstring_view, std::wstringstream>(str, outval);
+    return details::string::try_parse<std::wstring, std::wstring_view,
+                                      std::wstringstream>(str, outval);
+}
+
+// Specialization for const char* when we have different types
+// const char* could be converted to both stringview types.
+// So explicitly give a function taking const char*
+template <typename Str, typename T>
+bool try_parse(const Str str, T* outval)
+    requires(std::is_assignable_v<std::string_view, Str> &&
+             !std::is_same_v<absl::string_view, std::string_view> &&
+             !std::is_same_v<Str, std::string_view>)
+{
+    return details::string::try_parse<std::string, std::string_view,
+                                      std::stringstream>(str, outval);
+}
+
+// Specialization for absl::string_view, which isn't a std::string_view
+// in some cases
+template <typename T>
+bool try_parse(const absl::string_view str, T* outval)
+    requires(!std::is_same_v<absl::string_view, std::string_view>)
+{
+    return details::string::try_parse<std::string, absl::string_view,
+                                      std::stringstream>(str, outval);
 }
