6dbd665eb515325f0f12702e3d1bfeb351b23eb4
royna2544 <whiteshell2544@naver.com>
2024-11-29 15:24:50 +0900

    tests: Add popen_wdt test

---
+++ tests/PopenWdtTest.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+#include <popen_wdt.h>
+
+TEST(PopenWdtTest, Init) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    popen_watchdog_destroy(&data);
+}
+
+TEST(PopenWdtTest, NonBlockingCommand) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    data->watchdog_enabled = false;
+    data->command = "ls";
+    EXPECT_TRUE(popen_watchdog_start(&data));
+    EXPECT_FALSE(popen_watchdog_activated(&data));
+    auto ret = popen_watchdog_destroy(&data);
+    EXPECT_EQ(ret.exitcode, 0);
+    EXPECT_FALSE(ret.signal);
+}
+
+TEST(PopenWdtTest, BlockingCommand) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    data->watchdog_enabled = false;
+    data->sleep_secs = 1;
+    data->command = "sleep 3";
+    EXPECT_TRUE(popen_watchdog_start(&data));
+    EXPECT_FALSE(popen_watchdog_activated(&data));
+    auto ret = popen_watchdog_destroy(&data);
+    EXPECT_EQ(ret.exitcode, 0);
+    EXPECT_FALSE(ret.signal);
+}
+
+TEST(PopenWdtTest, NonBlockingCommandEnabled) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    data->watchdog_enabled = true;
+    data->sleep_secs = 4;
+    data->command = "ls";
+    EXPECT_TRUE(popen_watchdog_start(&data));
+    EXPECT_FALSE(popen_watchdog_activated(&data));
+    auto ret = popen_watchdog_destroy(&data);
+    EXPECT_EQ(ret.exitcode, 0);
+    EXPECT_FALSE(ret.signal);
+}
+
+TEST(PopenWdtTest, BlockingCommandEnabled) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    data->watchdog_enabled = true;
+    data->sleep_secs = 1;
+    data->command = "sleep 3";
+    EXPECT_TRUE(popen_watchdog_start(&data));
+    EXPECT_TRUE(popen_watchdog_activated(&data));
+    auto ret = popen_watchdog_destroy(&data);
+    EXPECT_EQ(ret.exitcode, POPEN_WDT_SIGTERM);
+    EXPECT_TRUE(ret.signal);
+}
+
+TEST(PopenWdtTest, NothingDestroy) {
+    popen_watchdog_data_t* data = nullptr;
+    auto ret = popen_watchdog_destroy(&data);
+    popen_watchdog_exit_t empty = POPEN_WDT_EXIT_INITIALIZER;
+    EXPECT_EQ(ret.exitcode, empty.exitcode);
+    EXPECT_EQ(ret.signal, empty.signal);
+}
