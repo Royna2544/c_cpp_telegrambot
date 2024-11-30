48cb94e6d7d5540ea424857363e7771fc12144e5
royna2544 <whiteshell2544@naver.com>
2024-11-30 17:09:19 +0900

    tests: PopenWdtTest: Add output test

--- tests/PopenWdtTest.cpp
+++ tests/PopenWdtTest.cpp
@@ -17,8 +17,23 @@ TEST(PopenWdtTest, NonBlockingCommand) {
     popen_watchdog_data_t* data = nullptr;
     EXPECT_TRUE(popen_watchdog_init(&data));
     data->watchdog_enabled = false;
-    data->command = "ls";
+    data->command = "pwd";
     ASSERT_TRUE(popen_watchdog_start(&data));
+    EXPECT_FALSE(popen_watchdog_activated(&data));
+    auto ret = popen_watchdog_destroy(&data);
+    EXPECT_EQ(ret.exitcode, 0);
+    EXPECT_FALSE(ret.signal);
+}
+
+TEST(PopenWdtTest, TestingEchoOutput) {
+    popen_watchdog_data_t* data = nullptr;
+    EXPECT_TRUE(popen_watchdog_init(&data));
+    data->watchdog_enabled = false;
+    data->command = "echo test";
+    ASSERT_TRUE(popen_watchdog_start(&data));
+    char buf[5] = {0};
+    EXPECT_TRUE(popen_watchdog_read(&data, buf, sizeof(buf) - 1));
+    EXPECT_STREQ(buf, "test");
     EXPECT_FALSE(popen_watchdog_activated(&data));
     auto ret = popen_watchdog_destroy(&data);
     EXPECT_EQ(ret.exitcode, 0);
@@ -43,7 +58,7 @@ TEST(PopenWdtTest, NonBlockingCommandEnabled) {
     EXPECT_TRUE(popen_watchdog_init(&data));
     data->watchdog_enabled = true;
     data->sleep_secs = 4;
-    data->command = "ls";
+    data->command = "pwd";
     ASSERT_TRUE(popen_watchdog_start(&data));
     EXPECT_FALSE(popen_watchdog_activated(&data));
     auto ret = popen_watchdog_destroy(&data);
