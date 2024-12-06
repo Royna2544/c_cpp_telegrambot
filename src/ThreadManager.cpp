53499b32460d4e44b77d86bae04e03f8b697aedd
royna2544 <whiteshell2544@naver.com>
2024-12-06 13:12:56 +0900

    ThreadManager: Add compability with MSYS

--- src/ThreadManager.cpp
+++ src/ThreadManager.cpp
@@ -17,9 +17,29 @@
 #endif
 
 #ifdef _WIN32
-#include <windows.h> // TerminateThread
+#include <windows.h>  // TerminateThread
 
 #define THREAD_FORCE_KILL_SUPPORTED
+#endif
+
+#ifdef THREAD_FORCE_KILL_SUPPORTED
+void kill_thread(std::thread::native_handle_type handle) {
+#ifdef _POSIX_C_SOURCE
+    pthread_kill(handle, SIGUSR1);
+#endif
+#ifdef _WIN32
+#ifdef _MSC_VER
+    TerminateThread(handle, 0);
+#else /* _MSC_VER */
+    HANDLE hThread =
+        OpenThread(THREAD_TERMINATE, FALSE, static_cast<DWORD>(handle));
+    if (hThread) {
+        TerminateThread(hThread, 0);
+        CloseHandle(hThread);  // Always close the handle after use
+    }
+#endif // _MSC_VER
+#endif // _WIN32
+}
 #endif
 
 constexpr std::chrono::seconds kShutdownDelay(5);
@@ -40,16 +60,13 @@ void ThreadManager::destroy() {
     std::unique_lock<std::mutex> lock(mutex);
     if (!condvar.wait_for(lock, kShutdownDelay,
                           [this] { return barrier.try_wait(); })) {
-        LOG(ERROR) << fmt::format("Timed out waiting for threads to finish (waited {})", kShutdownDelay);
+        LOG(ERROR) << fmt::format(
+            "Timed out waiting for threads to finish (waited {})",
+            kShutdownDelay);
         for (const auto& thread : kControllers) {
             if (thread.second && thread.second->running()) {
                 LOG(ERROR) << fmt::format("Killing thread {}", thread.first);
-#ifdef _POSIX_C_SOURCE
-                pthread_kill(thread.second->threadP.native_handle(), SIGUSR1);
-#endif
-#ifdef _WIN32
-                TerminateThread(thread.second->threadP.native_handle(), 0);
-#endif
+                kill_thread(thread.second->threadP.native_handle());
             }
         }
     }
