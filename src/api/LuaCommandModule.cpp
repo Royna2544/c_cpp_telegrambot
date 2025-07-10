bb525ed1a0e04658d18ce88b331db2a904a9529c
Royna2544 <whiteshell2544@naver.com>
2025-07-10 19:39:00 +0900

    api: Lua module fixes
    - Catch invalid script file exception
    - Unload/Load support
    - Fix inverted hide_description logic
    - Thread safety over sol::state
    - Export includepath
    - Fix including main codes

--- src/api/LuaCommandModule.cpp
+++ src/api/LuaCommandModule.cpp
@@ -11,6 +11,7 @@
 struct LuaCommandModule::Context {
     sol::state lua;
     std::filesystem::path filePath;
+    bool isLoaded = false;
 };
 
 LuaCommandModule::LuaCommandModule(std::filesystem::path filePath)
@@ -23,7 +24,13 @@ LuaCommandModule::~LuaCommandModule() = default;
 
 bool LuaCommandModule::load() {
     // Load script file
-    _context->lua.script_file(_context->filePath.string());
+    try {
+        _context->lua.script_file(_context->filePath.string());
+    } catch (const sol::error& ex) {
+        LOG(ERROR) << ex.what();
+        return false;
+    }
+
     // Make bindings
     sol::table meta = _context->lua["command"];
     info.name = meta["name"];
@@ -35,7 +42,7 @@ bool LuaCommandModule::load() {
     if (!permissive) {
         flags = flags | DynModule::Flags::Enforced;
     }
-    if (!hide_description) {
+    if (hide_description) {
         flags = flags | DynModule::Flags::HideDescription;
     }
     info.flags = flags;
@@ -44,14 +51,19 @@ bool LuaCommandModule::load() {
                         TgBotApi::Ptr api, MessageExt* message,
                         const StringResLoader::PerLocaleMap* res,
                         const Providers* provider) {
-        c->lua["reply"] = [api, message](const std::string_view text) {
+        sol::state_view lua = c->lua;
+
+        if (!c->isLoaded) return;
+
+        lua["reply"] = [api, message](const std::string_view text) {
             api->sendReplyMessage(message->message(), text);
         };
 
         // bind db helper
-        sol::table db = c->lua.create_table();
+        sol::table db = lua.create_table();
         db.set_function(
-            "get", [provider](const std::string& k, sol::this_state L) -> sol::object {
+            "get",
+            [provider](const std::string& k, sol::this_state L) -> sol::object {
                 if (k == "owner_id") {
                     auto val = provider->database->getOwnerUserId();
                     return val ? sol::make_object(L, *val) : sol::nil;
@@ -60,18 +72,24 @@ bool LuaCommandModule::load() {
             });
         db.set_function("set", [provider](const std::string& k, sol::object v) {
             if (v.is<int>())
-                if (k == "owner_id") provider->database->setOwnerUserId(v.as<int>());
+                if (k == "owner_id")
+                    provider->database->setOwnerUserId(v.as<int>());
         });
-        c->lua["db"] = db;
+        lua["db"] = db;
 
-        c->lua["run"]();
+        lua["run"]();
     };
 
     DLOG(INFO) << "Loaded Lua script: " << _context->filePath;
 
+    _context->isLoaded = true;
     return true;
 }
 
-bool LuaCommandModule::unload() { return false; }
+bool LuaCommandModule::unload() {
+    _context->lua = {};
+    _context->isLoaded = false;
+    return true;
+}
 
-bool LuaCommandModule::isLoaded() const { return true; }
+bool LuaCommandModule::isLoaded() const { return _context->isLoaded; }
