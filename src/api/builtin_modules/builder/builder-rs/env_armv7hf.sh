c5c40f18453466d0cd4dfc49c7ddec4757d6b394
Soo Hwan Na <whiteshell2544@naver.com>
2026-02-14 18:05:21 +0900

    Update ARMv7 cross-compilation flags for improved optimization and linking

--- src/api/builtin_modules/builder/builder-rs/env_armv7hf.sh
+++ src/api/builtin_modules/builder/builder-rs/env_armv7hf.sh
@@ -9,4 +9,6 @@ export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER="$CROSS_TOOLCHAIN_PREFI
     CROSS_CMAKE_SYSTEM_NAME=Linux \
     CROSS_CMAKE_SYSTEM_PROCESSOR=arm \
     CROSS_CMAKE_CRT=gnu \
-    CROSS_CMAKE_OBJECT_FLAGS="-ffunction-sections -fdata-sections -fPIC -march=armv7-a -mfpu=vfpv3-d16"
+    CROSS_CMAKE_OBJECT_FLAGS="-ffunction-sections -fdata-sections -fPIC -march=armv7-a -mfpu=neon -mfloat-abi=hard" \
+    CROSS_CMAKE_SHARED_LINKER_FLAGS="-Wl,--gc-sections -Wl,-z,now -Wl,-z,relro" \
+    CROSS_CMAKE_STATIC_LINKER_FLAGS="-Wl,--gc-sections -Wl,-z,now -Wl,-z,relro"
