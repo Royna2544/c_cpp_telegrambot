057831ee1b69865e20723892771e46cb1f562691
Soo Hwan Na <whiteshell2544@naver.com>
2026-02-21 19:26:02 +0900

    Update OpenCV components in CMake and enhance vcpkg configuration for image processing

--- src/imagep/ImageProcOpenCV.cpp
+++ src/imagep/ImageProcOpenCV.cpp
@@ -6,7 +6,6 @@
 #include <memory>
 #include <opencv2/core.hpp>
 #include <opencv2/core/utility.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
 
