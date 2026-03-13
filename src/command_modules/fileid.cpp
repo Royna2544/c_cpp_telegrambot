aa9b3ab27e4875621a36eac123ea31636de9c364
Soo-Hwan Na <whiteshell2544@naver.com>
2026-03-13 22:40:09 +0900

    Enhance /fileid: show thumbnail IDs, add video/doc support
    The /fileid command now extracts and displays thumbnail file IDs
    for stickers, animations, and videos when available. Support for
    video and document file IDs has also been added. The reply
    message conditionally includes the thumbnail file ID if present.

--- src/command_modules/fileid.cpp
+++ src/command_modules/fileid.cpp
@@ -9,24 +9,52 @@
 DECLARE_COMMAND_HANDLER(fileid) {
     std::string file;
     std::string unifile;
+    std::optional<std::string> thumbnail;
 
     if (message->reply()->exists()) {
         const auto reply = message->reply();
         if (reply->has<MessageAttrs::Sticker>()) {
             file = reply->get<MessageAttrs::Sticker>()->fileId;
             unifile = reply->get<MessageAttrs::Sticker>()->fileUniqueId;
+            thumbnail = reply->get<MessageAttrs::Sticker>()->thumbnail ? std::make_optional(
+                            reply->get<MessageAttrs::Sticker>()->thumbnail->fileId)
+                    : std::nullopt;
         } else if (reply->has<MessageAttrs::Animation>()) {
             file = reply->get<MessageAttrs::Animation>()->fileId;
             unifile = reply->get<MessageAttrs::Animation>()->fileUniqueId;
+            thumbnail = reply->get<MessageAttrs::Animation>()->thumbnail
+                            ? std::make_optional(
+                                  reply->get<MessageAttrs::Animation>()
+                                      ->thumbnail->fileId)
+                    : std::nullopt;
         } else if (reply->has<MessageAttrs::Photo>()) {
             file = reply->get<MessageAttrs::Photo>()->fileId;
             unifile = reply->get<MessageAttrs::Photo>()->fileUniqueId;
+        } else if (reply->has<MessageAttrs::Video>()) {
+            file = reply->get<MessageAttrs::Video>()->fileId;
+            unifile = reply->get<MessageAttrs::Video>()->fileUniqueId;
+            thumbnail =
+                reply->get<MessageAttrs::Video>()->thumbnail
+                    ? std::make_optional(
+                          reply->get<MessageAttrs::Video>()->thumbnail->fileId)
+                    : std::nullopt;
+        } else if (reply->has<MessageAttrs::Document>()) {
+            file = reply->get<MessageAttrs::Document>()->fileId;
+            unifile = reply->get<MessageAttrs::Document>()->fileUniqueId;
         } else {
             file = unifile = res->get(Strings::UNKNOWN);
         }
-        api->sendReplyMessage<TgBotApi::ParseMode::Markdown>(
-            message->message(),
-            fmt::format("FileId: `{}`\nFileUniqueId: `{}`", file, unifile));
+        if (thumbnail) {
+            api->sendReplyMessage<TgBotApi::ParseMode::Markdown>(
+                message->message(),
+                fmt::format(
+                    "FileId: `{}`\nFileUniqueId: `{}`\nThumbnail FileId: `{}`",
+                    file, unifile, *thumbnail));
+        } else {
+            api->sendReplyMessage<TgBotApi::ParseMode::Markdown>(
+                message->message(),
+                fmt::format("FileId: `{}`\nFileUniqueId: `{}`", file, unifile));
+        }
     } else {
         api->sendReplyMessage(message->message(),
                               res->get(Strings::REPLY_TO_A_MEDIA));
